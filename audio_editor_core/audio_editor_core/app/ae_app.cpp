9332e1f6cc4b5f43b75472108972950f8d04b612
Yuki Yasoshima <yukiyasos@gmail.com>
2021-04-16 22:03:20 +0900

    code format

--- audio_editor_core/audio_editor_core/app/ae_app.cpp
+++ audio_editor_core/audio_editor_core/app/ae_app.cpp
@@ -7,8 +7,9 @@
 using namespace yas;
 using namespace yas::ae;
 
-ae_app::ae_app() {}
+ae_app::ae_app() {
+}
 
 ae_app_ptr ae_app::make_shared() {
-  return std::shared_ptr<ae_app>(new ae_app{});
+    return std::shared_ptr<ae_app>(new ae_app{});
 }
--- audio_editor_core/audio_editor_core/app/ae_app.h
+++ audio_editor_core/audio_editor_core/app/ae_app.h
@@ -11,14 +11,14 @@ class ae_app;
 using ae_app_ptr = std::shared_ptr<ae_app>;
 
 struct ae_app {
-  ae_app_ptr make_shared();
+    ae_app_ptr make_shared();
 
-private:
-  ae_app();
+   private:
+    ae_app();
 
-  ae_app(ae_app const &) = delete;
-  ae_app(ae_app &&) = delete;
-  ae_app &operator=(ae_app const &) = delete;
-  ae_app &operator=(ae_app &&) = delete;
+    ae_app(ae_app const &) = delete;
+    ae_app(ae_app &&) = delete;
+    ae_app &operator=(ae_app const &) = delete;
+    ae_app &operator=(ae_app &&) = delete;
 };
-} // namespace yas::ae
+}  // namespace yas::ae
