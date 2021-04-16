0af3b358b5ae9e4cc7cfb839ba6998150829e6d5
Yuki Yasoshima <yukiyasos@gmail.com>
2021-04-16 20:58:29 +0900

    create audio_editor_core project
    fix

---
+++ audio_editor_core/audio_editor_core/audio_editor_core.h
@@ -0,0 +1,15 @@
+//
+//  audio_editor_core.h
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for audio_editor_core.
+FOUNDATION_EXPORT double audio_editor_coreVersionNumber;
+
+//! Project version string for audio_editor_core.
+FOUNDATION_EXPORT const unsigned char audio_editor_coreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <audio_editor_core/PublicHeader.h>
+
+
