f7f18bc5f6d298c1b81306d740d3a35c9eca4b3c
Yuki Yasoshima <yukiyasos@gmail.com>
2021-04-16 20:55:02 +0900

    create audio_editor project
    fix

---
+++ audio_editor/audio_editor/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  audio_editor
+//
+//  Created by 八十嶋祐樹 on 2021/04/16.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
