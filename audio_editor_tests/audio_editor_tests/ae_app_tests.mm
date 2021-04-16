2e5ae8dead2384f6a70f6481a9e9df9749cd7bd4
Yuki Yasoshima <yukiyasos@gmail.com>
2021-04-16 22:09:28 +0900

    add tests

--- audio_editor_tests/audio_editor_tests/ae_app_tests.mm
+++ audio_editor_tests/audio_editor_tests/ae_app_tests.mm
@@ -5,10 +5,19 @@
 #import <XCTest/XCTest.h>
 #import <audio_editor_core/audio_editor_core_umbrella.h>
 
+using namespace yas;
+using namespace yas::ae;
+
 @interface ae_app_tests : XCTestCase
 
 @end
 
 @implementation ae_app_tests
+
+- (void)test_make_shared {
+    auto app = ae_app::make_shared();
+
+    XCTAssertTrue(app != nullptr);
+}
 
 @end
