e787db6557797bc9c3f47d81b863e81b3fc87aea
Yuki Yasoshima <yukiyasos@gmail.com>
2021-04-17 18:13:07 +0900

    add tests

--- audio_editor_tests/ae_app_tests.mm
+++ audio_editor_tests/ae_app_tests.mm
@@ -15,9 +15,41 @@ using namespace yas::ae;
 @implementation ae_app_tests
 
 - (void)test_make_shared {
-    auto app = app::make_shared();
+    auto const app = app::make_shared();
 
     XCTAssertTrue(app != nullptr);
+    XCTAssertEqual(app->projects().size(), 0);
+}
+
+- (void)test_add_project {
+    auto const app = app::make_shared();
+
+    std::vector<app::projects_t::event> called;
+
+    auto canceller = app->observe_projects([&called](auto const &event) { called.emplace_back(event); }).sync();
+
+    XCTAssertEqual(called.size(), 1);
+    XCTAssertEqual(called.at(0).type, observing::vector::event_type::any);
+    XCTAssertEqual(called.at(0).elements.size(), 0);
+    XCTAssertEqual(app->projects().size(), 0);
+
+    auto const file_url_0 = url::file_url("/test/path/file0.wav");
+    app->add_project(file_url_0);
+
+    XCTAssertEqual(called.size(), 2);
+    XCTAssertEqual(called.at(1).type, observing::vector::event_type::inserted);
+    XCTAssertEqual(called.at(1).index, 0);
+    XCTAssertEqual(called.at(1).element->get()->file_url(), file_url_0);
+    XCTAssertEqual(app->projects().size(), 1);
+
+    auto const file_url_1 = url::file_url("/test/path/file1.aif");
+    app->add_project(file_url_1);
+
+    XCTAssertEqual(called.size(), 3);
+    XCTAssertEqual(called.at(2).type, observing::vector::event_type::inserted);
+    XCTAssertEqual(called.at(2).index, 1);
+    XCTAssertEqual(called.at(2).element->get()->file_url(), file_url_1);
+    XCTAssertEqual(app->projects().size(), 2);
 }
 
 @end
