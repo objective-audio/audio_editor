1b30d576671ab5a5f84cbb9411c47a53f66a331e
Yuki Yasoshima <yukiyasos@gmail.com>
2021-04-17 18:34:17 +0900

    add AEWindowController

---
+++ audio_editor/audio_editor/AEWindowController.h
@@ -0,0 +1,13 @@
+//
+//  AEWindowController.h
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AEWindowController : NSWindowController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ audio_editor/audio_editor/AEWindowController.mm
@@ -0,0 +1,20 @@
+//
+//  AEWindowController.mm
+//
+
+#import "AEWindowController.h"
+
+@interface AEWindowController ()
+
+@end
+
+@implementation AEWindowController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its
+    // nib file.
+}
+
+@end
