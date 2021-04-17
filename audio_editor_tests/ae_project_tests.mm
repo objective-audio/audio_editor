158e60e804d239f04c6a5d452ed46014b22410c4
Yuki Yasoshima <yukiyasos@gmail.com>
2021-04-17 16:46:10 +0900

    move test file

--- audio_editor_tests/ae_project_tests.mm
+++ audio_editor_tests/ae_project_tests.mm
@@ -5,10 +5,19 @@
 #import <XCTest/XCTest.h>
 #import <audio_editor_core/audio_editor_core_umbrella.h>
 
+using namespace yas;
+using namespace yas::ae;
+
 @interface ae_project_tests : XCTestCase
 
 @end
 
 @implementation ae_project_tests
+
+- (void)test_make_shared {
+    auto project = project::make_shared();
+
+    XCTAssertTrue(project != nullptr);
+}
 
 @end
