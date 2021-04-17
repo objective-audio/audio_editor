537aa701ac54c2da88303d5606921dcfdf44d32f
Yuki Yasoshima <yukiyasos@gmail.com>
2021-04-17 16:43:22 +0900

    add project tests

---
+++ audio_editor_mac_tests/audio_editor_mac_tests/ae_project_tests.mm
@@ -0,0 +1,14 @@
+//
+//  ae_project_tests.mm
+//
+
+#import <XCTest/XCTest.h>
+#import <audio_editor_core/audio_editor_core_umbrella.h>
+
+@interface ae_project_tests : XCTestCase
+
+@end
+
+@implementation ae_project_tests
+
+@end
