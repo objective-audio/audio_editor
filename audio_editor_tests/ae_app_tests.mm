b6bdda2b587f3abac6897ccf411bc4c75f213e1c
Yuki Yasoshima <yukiyasos@gmail.com>
2021-04-18 15:32:31 +0900

    fix tests

--- audio_editor_tests/ae_app_tests.mm
+++ audio_editor_tests/ae_app_tests.mm
@@ -29,7 +29,7 @@ using namespace yas::ae;
     auto canceller = app->observe_projects([&called](auto const &event) { called.emplace_back(event); }).sync();
 
     XCTAssertEqual(called.size(), 1);
-    XCTAssertEqual(called.at(0).type, observing::vector::event_type::any);
+    XCTAssertEqual(called.at(0).type, observing::map::event_type::any);
     XCTAssertEqual(called.at(0).elements.size(), 0);
     XCTAssertEqual(app->projects().size(), 0);
 
@@ -37,18 +37,16 @@ using namespace yas::ae;
     app->add_project(file_url_0);
 
     XCTAssertEqual(called.size(), 2);
-    XCTAssertEqual(called.at(1).type, observing::vector::event_type::inserted);
-    XCTAssertEqual(called.at(1).index, 0);
-    XCTAssertEqual(called.at(1).element->get()->file_url(), file_url_0);
+    XCTAssertEqual(called.at(1).type, observing::map::event_type::inserted);
+    XCTAssertEqual(called.at(1).inserted->get()->file_url(), file_url_0);
     XCTAssertEqual(app->projects().size(), 1);
 
     auto const file_url_1 = url::file_url("/test/path/file1.aif");
     app->add_project(file_url_1);
 
     XCTAssertEqual(called.size(), 3);
-    XCTAssertEqual(called.at(2).type, observing::vector::event_type::inserted);
-    XCTAssertEqual(called.at(2).index, 1);
-    XCTAssertEqual(called.at(2).element->get()->file_url(), file_url_1);
+    XCTAssertEqual(called.at(2).type, observing::map::event_type::inserted);
+    XCTAssertEqual(called.at(2).inserted->get()->file_url(), file_url_1);
     XCTAssertEqual(app->projects().size(), 2);
 }
 
@@ -61,11 +59,17 @@ using namespace yas::ae;
     auto const file_url_1 = url::file_url("/test/path/file1.aif");
     app->add_project(file_url_1);
 
-    app->projects().at(0)->request_close();
+    auto const pair0 =
+        yas::first(app->projects(), [&file_url_0](auto const &pair) { return pair.second->file_url() == file_url_0; });
+    XCTAssertTrue(pair0.has_value());
+    pair0.value().second->request_close();
     XCTAssertEqual(app->projects().size(), 1);
 
-    //    XCTAssertTrue(app->projects().at(0)->requestClose());
-    //    XCTAssertEqual(app->projects().size(), 0);
+    auto const pair1 =
+        yas::first(app->projects(), [&file_url_1](auto const &pair) { return pair.second->file_url() == file_url_1; });
+    XCTAssertTrue(pair1.has_value());
+    pair1.value().second->request_close();
+    XCTAssertEqual(app->projects().size(), 0);
 }
 
 @end
