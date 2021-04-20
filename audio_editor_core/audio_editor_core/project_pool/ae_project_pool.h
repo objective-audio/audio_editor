1a25b721fde7f3f62db13d0f487270f15562dd77
Yuki Yasoshima <yukiyasos@gmail.com>
2021-04-21 01:24:37 +0900

    project_pool

--- audio_editor_core/audio_editor_core/project_pool/ae_project_pool.h
+++ audio_editor_core/audio_editor_core/project_pool/ae_project_pool.h
@@ -8,6 +8,11 @@
 
 namespace yas::ae {
 struct project_pool {
+    project_ptr add_project(url const &file_url);
+    [[nodiscard]] project_ptr project_for_id(uintptr_t const) const;
+    [[nodiscard]] std::vector<project_ptr> projects() const;
+    [[nodiscard]] observing::syncable observe_project(std::function<void(project_pool_event const &)> &&);
+
     static project_pool_ptr make_shared();
 
    private:
--- audio_editor_core/audio_editor_core/project_pool/ae_project_pool_dependency.h
+++ audio_editor_core/audio_editor_core/project_pool/ae_project_pool_dependency.h
@@ -7,3 +7,15 @@
 #include <audio_editor_core/ae_ptr.h>
 #include <cpp_utils/yas_url.h>
 #include <observing/yas_observing_umbrella.h>
+
+namespace yas::ae {
+enum class project_pool_event_type {
+    inserted,
+    erased,
+};
+
+struct project_pool_event {
+    project_pool_event_type type;
+    uintptr_t project_id;
+};
+}  // namespace yas::ae
