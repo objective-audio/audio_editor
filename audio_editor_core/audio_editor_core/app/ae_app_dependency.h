1370437c838f490fda0860c0b192120f3f415385
Yuki Yasoshima <yukiyasos@gmail.com>
2021-04-21 01:38:30 +0900

    remove interface

--- audio_editor_core/audio_editor_core/app/ae_app_dependency.h
+++ audio_editor_core/audio_editor_core/app/ae_app_dependency.h
@@ -9,26 +9,4 @@
 #include <cpp_utils/yas_url.h>
 #include <observing/yas_observing_umbrella.h>
 
-namespace yas::ae {
-struct app_project_interface {
-    enum class notification {
-        should_close,
-    };
-
-    virtual ~app_project_interface() = default;
-
-    virtual uintptr_t identifier() const = 0;
-    virtual url const &file_url() const = 0;
-
-    virtual bool can_close() const = 0;
-    virtual void request_close() = 0;
-
-    virtual observing::endable observe_notification(std::function<void(notification const &)> &&) = 0;
-};
-
-struct app_factory_interface {
-    virtual ~app_factory_interface() = default;
-
-    virtual project_ptr make_project(url const &file_url) const = 0;
-};
-}  // namespace yas::ae
+namespace yas::ae {}  // namespace yas::ae
