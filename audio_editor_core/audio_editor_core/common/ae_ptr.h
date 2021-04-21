1ea4bbacfac4cada0185ea2e38ea7cf52f3952f0
Yuki Yasoshima <yukiyasos@gmail.com>
2021-04-21 21:09:52 +0900

    app_wptr

--- audio_editor_core/audio_editor_core/common/ae_ptr.h
+++ audio_editor_core/audio_editor_core/common/ae_ptr.h
@@ -13,6 +13,7 @@ class project_pool;
 class project;
 
 using app_ptr = std::shared_ptr<app>;
+using app_wptr = std::weak_ptr<app>;
 using project_pool_ptr = std::shared_ptr<project_pool>;
 using project_ptr = std::shared_ptr<project>;
 using project_wptr = std::weak_ptr<project>;
