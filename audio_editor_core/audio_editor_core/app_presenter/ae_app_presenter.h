6c41b2f6e0791c162cf5c54e6a5932c3291c6a87
Yuki Yasoshima <yukiyasos@gmail.com>
2021-04-23 23:55:25 +0900

    app_presenter event

--- audio_editor_core/audio_editor_core/app_presenter/ae_app_presenter.h
+++ audio_editor_core/audio_editor_core/app_presenter/ae_app_presenter.h
@@ -9,9 +9,6 @@
 
 namespace yas::ae {
 struct app_presenter {
-    using event_type = app_presenter_event_type;
-    using event = app_presenter_event;
-
     app_presenter();
     explicit app_presenter(std::shared_ptr<app_presenter_project_pool_interface> const &);
 
@@ -19,10 +16,11 @@ struct app_presenter {
     void open_file_dialog();
     void select_file(url const &);
 
-    [[nodiscard]] observing::syncable observe_event(std::function<void(event const &)> &&);
+    [[nodiscard]] observing::syncable observe_event(std::function<void(app_presenter_event const &)> &&);
 
    private:
     std::weak_ptr<app_presenter_project_pool_interface> _project_pool;
-    observing::notifier_ptr<event> const _event_notifier = observing::notifier<event>::make_shared();
+    observing::notifier_ptr<app_presenter_event> const _event_notifier =
+        observing::notifier<app_presenter_event>::make_shared();
 };
 }  // namespace yas::ae
--- audio_editor_core/audio_editor_core/app_presenter/ae_app_presenter_utils.h
+++ audio_editor_core/audio_editor_core/app_presenter/ae_app_presenter_utils.h
@@ -8,12 +8,12 @@
 #include <audio_editor_core/ae_project_pool_types.h>
 
 namespace yas::ae {
-static app_presenter::event_type to_presenter_event_type(project_pool_event_type const &type) {
+static app_presenter_event_type to_presenter_event_type(project_pool_event_type const &type) {
     switch (type) {
         case project_pool_event_type::inserted:
-            return app_presenter::event_type::make_and_show_window_controller;
+            return app_presenter_event_type::make_and_show_window_controller;
         case project_pool_event_type::erased:
-            return app_presenter::event_type::dispose_window_controller;
+            return app_presenter_event_type::dispose_window_controller;
     }
 }
 }  // namespace yas::ae
