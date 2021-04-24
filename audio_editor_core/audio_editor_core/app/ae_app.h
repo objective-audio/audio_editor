5fa062fa002fa2b82a0c5cd785b2ec9cea6b0e1e
Yuki Yasoshima <yukiyasos@gmail.com>
2021-04-25 00:23:28 +0900

    system url

--- audio_editor_core/audio_editor_core/app/ae_app.h
+++ audio_editor_core/audio_editor_core/app/ae_app.h
@@ -13,9 +13,11 @@ struct app final {
     [[nodiscard]] static std::shared_ptr<app> global();
 
     [[nodiscard]] std::shared_ptr<project_pool> const &project_pool() const;
+    [[nodiscard]] std::shared_ptr<system_url> const &system_url() const;
 
    private:
     std::shared_ptr<ae::project_pool> const _project_pool = project_pool::make_shared();
+    std::shared_ptr<ae::system_url> const _system_url = system_url::make_shared();
 
     app();
 
