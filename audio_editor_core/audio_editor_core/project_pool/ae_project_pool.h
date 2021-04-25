50cfb09461378a9cfd87e207188f22fd133921e6
Yuki Yasoshima <yukiyasos@gmail.com>
2021-04-25 10:48:24 +0900

    generate id

--- audio_editor_core/audio_editor_core/project_pool/ae_project_pool.h
+++ audio_editor_core/audio_editor_core/project_pool/ae_project_pool.h
@@ -10,6 +10,7 @@
 
 namespace yas::ae {
 class project;
+class id_generator;
 
 struct project_pool : app_presenter_project_pool_interface {
     void add_project(url const &file_url) override;
@@ -21,6 +22,7 @@ struct project_pool : app_presenter_project_pool_interface {
     static std::shared_ptr<project_pool> make_shared();
 
    private:
+    std::shared_ptr<id_generator> const _id_generator;
     using projects_map_t =
         observing::map::holder<uintptr_t, std::pair<std::shared_ptr<project>, observing::cancellable_ptr>>;
     std::shared_ptr<projects_map_t> const _projects = projects_map_t::make_shared();
