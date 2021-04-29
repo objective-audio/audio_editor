54e72be889b15950e3168a067a0e4ae0f0c725c3
Yuki Yasoshima <yukiyasos@gmail.com>
2021-04-29 14:56:42 +0900

    cancel

--- audio_editor_core/audio_editor_core/file_importer/ae_file_importer.h
+++ audio_editor_core/audio_editor_core/file_importer/ae_file_importer.h
@@ -15,6 +15,7 @@ class file_importer_resource;
 
 struct file_importer : project_file_importer_interface {
     void import(file_importing_context &&) override;
+    void cancel(std::string const &);
 
     static std::shared_ptr<file_importer> make_shared(workable_ptr const &, uint32_t const priority);
 
