90187d6ee97ff03ae492c1732c59908d7866df59
Yuki Yasoshima <yukiyasos@gmail.com>
2021-05-01 12:28:37 +0900

    app_directory

--- audio_editor_core/audio_editor_core/utils/ae_system_url.cpp
+++ audio_editor_core/audio_editor_core/utils/ae_system_url.cpp
@@ -13,8 +13,12 @@ url system_url::document_directory() const {
     return system_path_utils::directory_url(system_path_utils::dir::document);
 }
 
+url system_url::app_directory() const {
+    return this->document_directory().appending("audio_editor_app");
+}
+
 url system_url::project_directory(std::string const &project_id) const {
-    return this->document_directory().appending(project_id);
+    return this->app_directory().appending(project_id);
 }
 
 std::shared_ptr<system_url> system_url::make_shared() {
--- audio_editor_core/audio_editor_core/utils/ae_system_url.h
+++ audio_editor_core/audio_editor_core/utils/ae_system_url.h
@@ -9,6 +9,7 @@
 namespace yas::ae {
 struct system_url final {
     url document_directory() const;
+    url app_directory() const;
     url project_directory(std::string const &project_id) const;
 
     static std::shared_ptr<system_url> make_shared();
