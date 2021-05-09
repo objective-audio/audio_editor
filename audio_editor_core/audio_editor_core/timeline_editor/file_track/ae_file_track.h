e2c5c967e31044dcaa377e0842e6c0ec91ca1815
Yuki Yasoshima <yukiyasos@gmail.com>
2021-05-09 10:11:25 +0900

    _modules

--- audio_editor_core/audio_editor_core/timeline_editor/file_track/ae_file_track.h
+++ audio_editor_core/audio_editor_core/timeline_editor/file_track/ae_file_track.h
@@ -4,13 +4,18 @@
 
 #pragma once
 
+#include <audio_editor_core/ae_file_module.h>
 #include <audio_editor_core/ae_timeline_editor_dependency.h>
+
+#include <map>
 
 namespace yas::ae {
 struct file_track final : timeline_file_track_interface {
     static std::shared_ptr<file_track> make_shared();
 
    private:
+    std::map<proc::time::range, file_module> _modules;
+
     file_track() = default;
 };
 }  // namespace yas::ae
