aeeadc72aee82f6fc08a0d6a946cc50c041154bc
Yuki Yasoshima <yukiyasos@gmail.com>
2021-05-11 23:46:38 +0900

    file_module_tests

--- audio_editor_tests/ae_file_module_tests.mm
+++ audio_editor_tests/ae_file_module_tests.mm
@@ -14,4 +14,22 @@ using namespace yas::ae;
 
 @implementation ae_file_module_tests
 
+- (void)test_head_dropped {
+    file_module const module{.range = {1, 3}, .file_frame = 10};
+
+    XCTAssertEqual(module.head_dropped(1), std::nullopt);
+    XCTAssertEqual(module.head_dropped(2), (file_module{.range = {2, 2}, .file_frame = 11}));
+    XCTAssertEqual(module.head_dropped(3), (file_module{.range = {3, 1}, .file_frame = 12}));
+    XCTAssertEqual(module.head_dropped(4), std::nullopt);
+}
+
+- (void)test_tail_dropped {
+    file_module const module{.range = {1, 3}, .file_frame = 10};
+
+    XCTAssertEqual(module.tail_dropped(1), std::nullopt);
+    XCTAssertEqual(module.tail_dropped(2), (file_module{.range = {1, 1}, .file_frame = 10}));
+    XCTAssertEqual(module.tail_dropped(3), (file_module{.range = {1, 2}, .file_frame = 10}));
+    XCTAssertEqual(module.tail_dropped(4), std::nullopt);
+}
+
 @end
