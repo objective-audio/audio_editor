85f1fe0eb70a5dc6c716b4a2630c749b2914b7cb
Yuki Yasoshima <yukiyasos@gmail.com>
2021-05-15 21:36:22 +0900

    remove file_module define

--- audio_editor_core/audio_editor_core/file_track/ae_file_module_utils.h
+++ audio_editor_core/audio_editor_core/file_track/ae_file_module_utils.h
@@ -7,10 +7,6 @@
 #include <audio_editor_core/ae_file_track_types.h>
 #include <processing/yas_processing_time.h>
 
-namespace yas::ae {
-class file_module;
-}
-
 namespace yas::ae::file_module_utils {
 std::optional<file_module> module(file_track_module_map_t const &, proc::frame_index_t const);
 std::optional<file_module> previous_module(file_track_module_map_t const &, proc::frame_index_t const);
