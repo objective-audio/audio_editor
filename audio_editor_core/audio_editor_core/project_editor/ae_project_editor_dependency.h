e199a911a57f12755a4b8ed7e8e211ecf14c8e49
Yuki Yasoshima <yukiyasos@gmail.com>
2021-05-16 12:26:29 +0900

    override

--- audio_editor_core/audio_editor_core/project_editor/ae_project_editor_dependency.h
+++ audio_editor_core/audio_editor_core/project_editor/ae_project_editor_dependency.h
@@ -47,6 +47,8 @@ struct project_editor_file_track_interface {
     virtual void drop_tail(proc::frame_index_t const) = 0;
     virtual void drop_head_and_offset(proc::frame_index_t const) = 0;
     virtual void drop_tail_and_offset(proc::frame_index_t const) = 0;
+    virtual void overwrite_module(file_module const &) = 0;
+    virtual void move_modules(std::set<proc::time::range> const &, proc::frame_index_t const) = 0;
 
     [[nodiscard]] virtual observing::syncable observe_event(std::function<void(file_track_event const &)> &&) = 0;
 };
