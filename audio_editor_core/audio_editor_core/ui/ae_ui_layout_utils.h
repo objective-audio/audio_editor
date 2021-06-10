7a89dd4e461092438596ca8ff4648ace0d9d0aec
Yuki Yasoshima <yukiyasos@gmail.com>
2021-06-11 00:06:15 +0900

    remove unused func

--- audio_editor_core/audio_editor_core/ui/ae_ui_layout_utils.h
+++ audio_editor_core/audio_editor_core/ui/ae_ui_layout_utils.h
@@ -7,11 +7,6 @@
 #include <ui/yas_ui_types.h>
 
 namespace yas::ae::ui_layout_utils {
-float equal_value(float const &);
-ui::point equal_point(ui::point const &);
-ui::range equal_range(ui::range const &);
-ui::region equal_region(ui::region const &);
-
 std::function<float(float const &)> constant(float const distance);
 std::function<ui::point(ui::point const &)> constant(ui::point const &distance);
 std::function<ui::range(ui::range const &)> constant(ui::range_insets const &distance);
