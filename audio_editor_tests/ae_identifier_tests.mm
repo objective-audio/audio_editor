ad243999dd2c8764b39b1243a6de1916fd4e07e2
Yuki Yasoshima <yukiyasos@gmail.com>
2021-12-04 11:20:04 +0900

    tests

---
+++ audio_editor_tests/ae_identifier_tests.mm
@@ -0,0 +1,41 @@
+//
+//  ae_identifier_tests.mm
+//
+
+#import <XCTest/XCTest.h>
+#include <audio_editor_core/ae_identifier.h>
+
+using namespace yas;
+using namespace yas::ae;
+
+@interface ae_identifier_tests : XCTestCase
+
+@end
+
+@implementation ae_identifier_tests
+
+- (void)test_equal_copied {
+    identifier const id_1{};
+    identifier const copied_id_1 = id_1;
+
+    XCTAssertEqual(id_1, copied_id_1);
+}
+
+- (void)test_not_equal_other {
+    identifier const id_1{};
+    identifier const id_2{};
+
+    XCTAssertNotEqual(id_1, id_2);
+}
+
+- (void)test_not_equal_moved {
+    identifier id_1{};
+    identifier moved_id_1 = std::move(id_1);
+    identifier id_2{};
+    identifier moved_id_2 = std::move(id_2);
+
+    XCTAssertNotEqual(id_1, moved_id_1);
+    XCTAssertNotEqual(id_1, id_2);
+}
+
+@end
