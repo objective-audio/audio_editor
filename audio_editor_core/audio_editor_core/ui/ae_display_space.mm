45db1fdcfceeff8a99886a901f46fc8836907f6c
Yuki Yasoshima <yukiyasos@gmail.com>
2021-12-11 22:46:00 +0900

    display_space

--- audio_editor_core/audio_editor_core/ui/ae_display_space.mm
+++ audio_editor_core/audio_editor_core/ui/ae_display_space.mm
@@ -12,28 +12,34 @@ std::shared_ptr<display_space> display_space::make_shared(ui::region const view_
 }
 
 display_space::display_space(ui::region const view_region)
-    : _view_region(view_region),
-      _scale({1.0f, 1.0f}),
-      _region(observing::value::holder<ui::region>::make_shared(ui::region::zero())) {
-    this->_update();
+    : _region(ui::region::zero()), _view_region(view_region), _scale({1.0f, 1.0f}) {
+    this->_fetcher = observing::fetcher<display_space_event>::make_shared([this] {
+        return display_space_event{
+            .source = display_space_event_source::fetched, .region = this->_region, .scale = this->_scale};
+    });
+    this->_update_region_and_notify(display_space_event_source::fetched);
 }
 
 void display_space::set_view_region(ui::region const view_region) {
     this->_view_region = view_region;
-    this->_update();
+    this->_update_region_and_notify(display_space_event_source::view);
 }
 
 void display_space::set_scale(ui::size const scale) {
     this->_scale = scale;
-    this->_update();
+    this->_update_region_and_notify(display_space_event_source::scale);
 }
 
 ui::region display_space::region() const {
-    return this->_region->value();
+    return this->_region;
 }
 
-observing::syncable display_space::observe_region(std::function<void(ui::region const &)> &&handler) {
-    return this->_region->observe(std::move(handler));
+observing::syncable display_space::observe(std::function<void(display_space_event const &)> &&handler) {
+    return this->_fetcher->observe(std::move(handler));
+}
+
+ui::size const &display_space::scale() const {
+    return this->_scale;
 }
 
 proc::time::range display_space::frame_range(uint32_t const sample_rate,
@@ -46,20 +52,25 @@ proc::time::range display_space::frame_range(uint32_t const sample_rate,
     return proc::time::range{min_edge_frame, static_cast<proc::length_t>(max_edge_frame - min_edge_frame)};
 }
 
-void display_space::_update() {
+void display_space::_update_region_and_notify(display_space_event_source const source) {
+    ui::region const prev_region = this->_region;
+
     if (this->_scale.width == 0 || this->_scale.height == 0) {
-        return;
+        this->_region = ui::region::zero();
+    } else {
+        auto const x_scale = 1.0f / this->_scale.width;
+        auto const y_scale = 1.0f / this->_scale.height;
+        auto const insets = this->_view_region.insets();
+
+        auto const left = insets.left * x_scale;
+        auto const right = insets.right * x_scale;
+        auto const top = insets.top * y_scale;
+        auto const bottom = insets.bottom * y_scale;
+
+        this->_region = {.origin = {.x = left, .y = bottom}, .size = {.width = right - left, .height = top - bottom}};
     }
 
-    auto const x_scale = 1.0f / this->_scale.width;
-    auto const y_scale = 1.0f / this->_scale.height;
-    auto const insets = this->_view_region.insets();
-
-    auto const left = insets.left * x_scale;
-    auto const right = insets.right * x_scale;
-    auto const top = insets.top * y_scale;
-    auto const bottom = insets.bottom * y_scale;
-
-    this->_region->set_value(
-        ui::region{.origin = {.x = left, .y = bottom}, .size = {.width = right - left, .height = top - bottom}});
+    if (prev_region != this->_region) {
+        this->_fetcher->push({.source = source, .region = this->_region, .scale = this->_scale});
+    }
 }
