c66de8cb157885f8b78964fb60d0c73a407ac92c
Yuki Yasoshima <yukiyasos@gmail.com>
2021-12-15 18:40:38 +0900

    module_location range

--- audio_editor_core/audio_editor_core/presenters/modules_presenter/ae_module_location.cpp
+++ audio_editor_core/audio_editor_core/presenters/modules_presenter/ae_module_location.cpp
@@ -11,5 +11,5 @@ module_location module_location::make(ae::identifier const &identifier, proc::ti
                                       uint32_t const sample_rate) {
     float const x = static_cast<double>(range.frame) / static_cast<double>(sample_rate);
     float const width = static_cast<double>(range.length) / static_cast<double>(sample_rate);
-    return module_location{identifier, x, width};
+    return module_location{identifier, x, width, range};
 }
