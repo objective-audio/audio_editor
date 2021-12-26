a83072fd9bc8735cf09bee096311b44e60f91f29
Yuki Yasoshima <yukiyasos@gmail.com>
2021-12-26 18:15:45 +0900

    fix tests

--- audio_editor_tests/ae_module_location_tests.mm
+++ audio_editor_tests/ae_module_location_tests.mm
@@ -19,26 +19,32 @@ using namespace yas::ae;
     identifier const id_2;
     proc::time::range const range_1(0, 1);
     proc::time::range const range_2(0, 2);
+    proc::frame_index_t const file_frame_1 = 1;
+    proc::frame_index_t const file_frame_2 = 2;
     uint32_t const sample_rate_1 = 1;
     uint32_t const sample_rate_2 = 2;
 
-    XCTAssertTrue(module_location::make_value(id_1, range_1, sample_rate_1) ==
-                  module_location::make_value(id_1, range_1, sample_rate_1));
-    XCTAssertFalse(module_location::make_value(id_1, range_1, sample_rate_1) ==
-                   module_location::make_value(id_2, range_1, sample_rate_1));
-    XCTAssertFalse(module_location::make_value(id_1, range_1, sample_rate_1) ==
-                   module_location::make_value(id_1, range_2, sample_rate_1));
-    XCTAssertFalse(module_location::make_value(id_1, range_1, sample_rate_1) ==
-                   module_location::make_value(id_1, range_1, sample_rate_2));
+    XCTAssertTrue(module_location::make_value(id_1, range_1, file_frame_1, sample_rate_1) ==
+                  module_location::make_value(id_1, range_1, file_frame_1, sample_rate_1));
+    XCTAssertFalse(module_location::make_value(id_1, range_1, file_frame_1, sample_rate_1) ==
+                   module_location::make_value(id_2, range_1, file_frame_1, sample_rate_1));
+    XCTAssertFalse(module_location::make_value(id_1, range_1, file_frame_1, sample_rate_1) ==
+                   module_location::make_value(id_1, range_2, file_frame_1, sample_rate_1));
+    XCTAssertFalse(module_location::make_value(id_1, range_1, file_frame_1, sample_rate_1) ==
+                   module_location::make_value(id_1, range_1, file_frame_2, sample_rate_1));
+    XCTAssertFalse(module_location::make_value(id_1, range_1, file_frame_1, sample_rate_1) ==
+                   module_location::make_value(id_1, range_1, file_frame_1, sample_rate_2));
 
-    XCTAssertFalse(module_location::make_value(id_1, range_1, sample_rate_1) !=
-                   module_location::make_value(id_1, range_1, sample_rate_1));
-    XCTAssertTrue(module_location::make_value(id_1, range_1, sample_rate_1) !=
-                  module_location::make_value(id_2, range_1, sample_rate_1));
-    XCTAssertTrue(module_location::make_value(id_1, range_1, sample_rate_1) !=
-                  module_location::make_value(id_1, range_2, sample_rate_1));
-    XCTAssertTrue(module_location::make_value(id_1, range_1, sample_rate_1) !=
-                  module_location::make_value(id_1, range_1, sample_rate_2));
+    XCTAssertFalse(module_location::make_value(id_1, range_1, file_frame_1, sample_rate_1) !=
+                   module_location::make_value(id_1, range_1, file_frame_1, sample_rate_1));
+    XCTAssertTrue(module_location::make_value(id_1, range_1, file_frame_1, sample_rate_1) !=
+                  module_location::make_value(id_2, range_1, file_frame_1, sample_rate_1));
+    XCTAssertTrue(module_location::make_value(id_1, range_1, file_frame_1, sample_rate_1) !=
+                  module_location::make_value(id_1, range_2, file_frame_1, sample_rate_1));
+    XCTAssertTrue(module_location::make_value(id_1, range_1, file_frame_1, sample_rate_1) !=
+                  module_location::make_value(id_1, range_1, file_frame_2, sample_rate_1));
+    XCTAssertTrue(module_location::make_value(id_1, range_1, file_frame_1, sample_rate_1) !=
+                  module_location::make_value(id_1, range_1, file_frame_1, sample_rate_2));
 }
 
 @end
