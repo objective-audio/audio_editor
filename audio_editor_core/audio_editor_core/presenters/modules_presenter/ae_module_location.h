5aa2f3d6edb671b7d7a8ba38e8956a3c58492265
Yuki Yasoshima <yukiyasos@gmail.com>
2021-12-26 13:23:06 +0900

    equal

--- audio_editor_core/audio_editor_core/presenters/modules_presenter/ae_module_location.h
+++ audio_editor_core/audio_editor_core/presenters/modules_presenter/ae_module_location.h
@@ -10,17 +10,32 @@
 
 namespace yas::ae {
 struct module_location {
+    struct mesh_element {
+        proc::time::range range;
+
+        bool operator==(mesh_element const &rhs) const {
+            return this->range == rhs.range;
+        }
+
+        bool operator!=(mesh_element const &rhs) const {
+            return !(*this == rhs);
+        }
+    };
+
     identifier identifier;  // file_moduleからコピーする
     float x;
     float width;
     proc::time::range range;
+    std::vector<std::optional<mesh_element>> mesh_elements;
 
     [[nodiscard]] static module_location make_value(ae::identifier const &, proc::time::range const &,
                                                     uint32_t const sample_rate);
 
     bool operator==(module_location const &rhs) const {
         return this->identifier == rhs.identifier && this->x == rhs.x && this->width == rhs.width &&
-               this->range == rhs.range;
+               this->range == rhs.range &&
+               std::equal(this->mesh_elements.begin(), this->mesh_elements.end(), rhs.mesh_elements.begin(),
+                          rhs.mesh_elements.end());
     }
 
     bool operator!=(module_location const &rhs) const {
