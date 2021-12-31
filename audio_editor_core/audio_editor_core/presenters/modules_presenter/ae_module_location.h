b76c9658fad838826c29f4881f09a1519391e8e9
Yuki Yasoshima <yukiyasos@gmail.com>
2021-12-31 11:01:15 +0900

    module_location

--- audio_editor_core/audio_editor_core/presenters/modules_presenter/ae_module_location.h
+++ audio_editor_core/audio_editor_core/presenters/modules_presenter/ae_module_location.h
@@ -15,6 +15,7 @@ struct module_location {
     struct mesh_element {
         static uint32_t const max_length = 32;
 
+        uint32_t rect_count;
         proc::time::range range;
 
         bool operator==(mesh_element const &rhs) const;
@@ -26,15 +27,20 @@ struct module_location {
     proc::frame_index_t file_frame;
     uint32_t sample_rate;
     std::vector<std::optional<mesh_element>> mesh_elements;
+    float width_per_sec;
 
     [[nodiscard]] static module_location make_value(ae::identifier const &, proc::time::range const &,
                                                     proc::frame_index_t const file_frame, uint32_t const sample_rate,
-                                                    std::vector<std::optional<mesh_element>> const &mesh_elements);
+                                                    std::vector<std::optional<mesh_element>> const &mesh_elements,
+                                                    float const width_per_sec);
     [[nodiscard]] static module_location make_value(file_module const &, uint32_t const sample_rate,
                                                     proc::time::range const &space_range, float const width_per_sec);
 
     float x() const;
     float width() const;
+    uint32_t total_rect_count() const;
+
+    std::optional<float> element_offset_x(std::size_t const) const;
 
     bool operator==(module_location const &rhs) const;
     bool operator!=(module_location const &rhs) const;
