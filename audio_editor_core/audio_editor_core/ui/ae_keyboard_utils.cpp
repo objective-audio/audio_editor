3e5026dfc3663ef1d12ae28594c9ea0b90befab1
Yuki Yasoshima <yukiyasos@gmail.com>
2022-01-03 10:37:35 +0900

    to_modifier

--- audio_editor_core/audio_editor_core/ui/ae_keyboard_utils.cpp
+++ audio_editor_core/audio_editor_core/ui/ae_keyboard_utils.cpp
@@ -46,3 +46,24 @@ std::optional<key> keyboard_utils::to_key(uint16_t const key_code) {
             return std::nullopt;
     }
 }
+
+ae::modifier keyboard_utils::to_modifier(ui::modifier_flags const flag) {
+    switch (flag) {
+        case ui::modifier_flags::alpha_shift:
+            return ae::modifier::alpha_shift;
+        case ui::modifier_flags::shift:
+            return ae::modifier::shift;
+        case ui::modifier_flags::control:
+            return ae::modifier::control;
+        case ui::modifier_flags::alternate:
+            return ae::modifier::alternate;
+        case ui::modifier_flags::command:
+            return ae::modifier::command;
+        case ui::modifier_flags::numeric_pad:
+            return ae::modifier::numeric_pad;
+        case ui::modifier_flags::help:
+            return ae::modifier::help;
+        case ui::modifier_flags::function:
+            return ae::modifier::function;
+    }
+}
--- audio_editor_core/audio_editor_core/ui/ae_keyboard_utils.h
+++ audio_editor_core/audio_editor_core/ui/ae_keyboard_utils.h
@@ -5,10 +5,12 @@
 #pragma once
 
 #include <audio_editor_core/ae_keyboard_types.h>
+#include <ui/yas_ui_event_types.h>
 
 #include <cstdint>
 #include <optional>
 
 namespace yas::ae::keyboard_utils {
 std::optional<key> to_key(uint16_t const key_code);
-}
+ae::modifier to_modifier(ui::modifier_flags const flag);
+}  // namespace yas::ae::keyboard_utils
