16bf7d38e48292ee2503ba78296a7848dc9f3669
Yuki Yasoshima <yukiyasos@gmail.com>
2022-01-09 08:22:42 +0900

    rename fraction_digits

--- audio_editor_core/audio_editor_core/presenters/time_presenter/ae_time_presenter_utils.cpp
+++ audio_editor_core/audio_editor_core/presenters/time_presenter/ae_time_presenter_utils.cpp
@@ -12,28 +12,28 @@
 using namespace yas;
 using namespace yas::ae;
 
-int time_presenter_utils::after_point_digits(uint32_t const sample_rate) {
-    if (sample_rate == 0) {
+int time_presenter_utils::fraction_digits(uint32_t const max) {
+    if (max == 0) {
         throw std::invalid_argument("sample_rate is zero.");
     }
 
-    if (sample_rate == 1) {
+    if (max == 1) {
         return 0;
     }
 
     uint32_t value = 10;
     uint32_t digits = 1;
-    uint32_t prev_mod = sample_rate;
+    uint32_t prev_mod = max;
 
     while (true) {
-        if ((sample_rate / value) == 0) {
+        if ((max / value) == 0) {
             if (prev_mod == 0) {
                 digits -= 1;
             }
             break;
         }
 
-        prev_mod = sample_rate % value;
+        prev_mod = max % value;
 
         digits += 1;
         value *= 10;
@@ -45,9 +45,9 @@ int time_presenter_utils::after_point_digits(uint32_t const sample_rate) {
 std::string time_presenter_utils::time_text(int64_t const frame, uint32_t const sample_rate) {
     int64_t const abs_frame = std::abs(frame);
 
-    auto const digits = after_point_digits(sample_rate);
+    auto const digits = fraction_digits(sample_rate);
 
-    int64_t const after_point = playing::math::mod_int(abs_frame, sample_rate);
+    int64_t const fraction = playing::math::mod_int(abs_frame, sample_rate);
     int64_t const sec = playing::math::floor_int(abs_frame, sample_rate) / sample_rate;
     int64_t const mod_sec = playing::math::mod_int(sec, 60);
     int64_t const min = sec / 60;
@@ -63,7 +63,7 @@ std::string time_presenter_utils::time_text(int64_t const frame, uint32_t const 
 
     if (digits > 0) {
         stream << ".";
-        stream << std::setw(digits) << std::setfill('0') << after_point;
+        stream << std::setw(digits) << std::setfill('0') << fraction;
     }
 
     return stream.str();
