a5e2ed8268a4766b29f8c0254c71b16803e02c54
Yuki Yasoshima <yukiyasos@gmail.com>
2022-01-31 22:28:34 +0900

    nodiscard

--- audio_editor_core/audio_editor_core/project/timing/ae_timing_types.h
+++ audio_editor_core/audio_editor_core/project/timing/ae_timing_types.h
@@ -36,20 +36,20 @@ struct timing_components {
     timing_components(number_components const &);
     timing_components(args const &);
 
-    number_components const &raw_components() const;
+    [[nodiscard]] number_components const &raw_components() const;
 
-    bool is_minus() const;
-    uint32_t value(timing_unit_kind const) const;
-    std::size_t fraction_unit_size() const;
+    [[nodiscard]] bool is_minus() const;
+    [[nodiscard]] uint32_t value(timing_unit_kind const) const;
+    [[nodiscard]] std::size_t fraction_unit_size() const;
 
-    bool is_zero() const;
-    timing_components abs() const;
+    [[nodiscard]] bool is_zero() const;
+    [[nodiscard]] timing_components abs() const;
 
     bool operator==(timing_components const &rhs) const;
     bool operator!=(timing_components const &rhs) const;
     bool operator<(timing_components const &) const;
 
-    timing_components adding(timing_components const &) const;
+    [[nodiscard]] timing_components adding(timing_components const &) const;
 
    private:
     number_components _components;
