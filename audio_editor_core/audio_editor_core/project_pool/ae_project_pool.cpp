2eb7de173c86508ece09e62010f577f96982251a
Yuki Yasoshima <yukiyasos@gmail.com>
2022-02-24 21:56:58 +0900

    nodiscard

--- audio_editor_core/audio_editor_core/project_pool/ae_project_pool.cpp
+++ audio_editor_core/audio_editor_core/project_pool/ae_project_pool.cpp
@@ -20,27 +20,28 @@ void project_pool::add_project(url const &file_url) {
 }
 
 std::shared_ptr<project> project_pool::add_and_return_project(url const &file_url) {
-    auto const project_level = this->_project_maker->make(file_url);
+    auto project_level = this->_project_maker->make(file_url);
     auto const &project = project_level->project;
 
     auto canceller = project
                          ->observe_event([this, project_id = project->identifier()](auto const &event) {
                              switch (event) {
                                  case project_event::should_close: {
-                                     this->_projects->erase(project_id);
+                                     this->_project_levels->erase(project_id);
                                  } break;
                              }
                          })
                          .end();
 
-    this->_projects->insert_or_replace(project->identifier(), std::make_pair(project, std::move(canceller)));
+    this->_project_levels->insert_or_replace(project->identifier(),
+                                             std::make_pair(std::move(project_level), std::move(canceller)));
 
     return project;
 }
 
 std::shared_ptr<project> const &project_pool::project_for_id(std::string const &project_id) const {
-    if (this->_projects->contains(project_id)) {
-        return this->_projects->at(project_id).first;
+    if (this->_project_levels->contains(project_id)) {
+        return this->_project_levels->at(project_id).first->project;
     } else {
         static std::shared_ptr<project> const empty_project = nullptr;
         return empty_project;
@@ -48,26 +49,27 @@ std::shared_ptr<project> const &project_pool::project_for_id(std::string const &
 }
 
 std::size_t project_pool::size() const {
-    return this->_projects->size();
+    return this->_project_levels->size();
 }
 
 observing::syncable project_pool::observe_event(std::function<void(project_pool_event const &)> &&handler) {
-    return this->_projects->observe([handler = std::move(handler)](project_pool::projects_map_t::event const &event) {
+    return this->_project_levels->observe([handler =
+                                               std::move(handler)](project_pool::project_levels_t::event const &event) {
         switch (event.type) {
             case observing::map::event_type::any: {
                 for (auto const &pair : event.elements) {
-                    handler({.type = project_pool_event_type::inserted, .project_id = pair.second.first->identifier()});
+                    handler({.type = project_pool_event_type::inserted, .project_id = pair.second.first->identifier});
                 }
             } break;
             case observing::map::event_type::inserted: {
-                handler({.type = project_pool_event_type::inserted, .project_id = event.inserted->first->identifier()});
+                handler({.type = project_pool_event_type::inserted, .project_id = event.inserted->first->identifier});
             } break;
             case observing::map::event_type::replaced: {
-                handler({.type = project_pool_event_type::erased, .project_id = event.erased->first->identifier()});
-                handler({.type = project_pool_event_type::inserted, .project_id = event.inserted->first->identifier()});
+                handler({.type = project_pool_event_type::erased, .project_id = event.erased->first->identifier});
+                handler({.type = project_pool_event_type::inserted, .project_id = event.inserted->first->identifier});
             } break;
             case observing::map::event_type::erased: {
-                handler({.type = project_pool_event_type::erased, .project_id = event.erased->first->identifier()});
+                handler({.type = project_pool_event_type::erased, .project_id = event.erased->first->identifier});
             } break;
         }
     });
