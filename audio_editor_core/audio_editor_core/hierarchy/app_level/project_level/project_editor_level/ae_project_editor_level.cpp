7f703306f07fda35b3f9efd8b66ccae4507f3d14
Yuki Yasoshima <yukiyasos@gmail.com>
2022-02-26 18:45:38 +0900

    project_editor_level

--- audio_editor_core/audio_editor_core/hierarchy/app_level/project_level/project_editor_level/ae_project_editor_level.cpp
+++ audio_editor_core/audio_editor_core/hierarchy/app_level/project_level/project_editor_level/ae_project_editor_level.cpp
@@ -4,8 +4,17 @@
 
 #include "ae_project_editor_level.h"
 
+#include <audio_editor_core/ae_database.h>
+#include <audio_editor_core/ae_edge_editor.h>
+#include <audio_editor_core/ae_exporter.h>
+#include <audio_editor_core/ae_file_track.h>
+#include <audio_editor_core/ae_hierarchy.h>
+#include <audio_editor_core/ae_marker_pool.h>
 #include <audio_editor_core/ae_nudging.h>
+#include <audio_editor_core/ae_pasteboard.h>
 #include <audio_editor_core/ae_project_editor.h>
+#include <audio_editor_core/ae_project_url.h>
+#include <audio_editor_core/ae_time_editor_maker.h>
 #include <audio_editor_core/ae_timing.h>
 
 using namespace yas;
@@ -13,13 +22,23 @@ using namespace yas::ae;
 
 std::shared_ptr<project_editor_level> project_editor_level::make_shared(std::string const &identifier,
                                                                         ae::file_info const &file_info) {
-    return std::shared_ptr<project_editor_level>(new project_editor_level{identifier, file_info});
+    auto const &project_level = hierarchy::project_level_for_id(identifier);
+    return std::shared_ptr<project_editor_level>(
+        new project_editor_level{identifier, file_info, project_level->project_url});
 }
 
-project_editor_level::project_editor_level(std::string const &identifier, ae::file_info const &file_info)
+project_editor_level::project_editor_level(std::string const &identifier, ae::file_info const &file_info,
+                                           std::shared_ptr<project_url> const &project_url)
     : identifier(identifier),
       file_info(file_info),
       timing(timing::make_shared(file_info.sample_rate)),
       nudging(nudging::make_shared(this->timing)),
+      file_track(file_track::make_shared()),
+      marker_pool(marker_pool::make_shared()),
+      edge_editor(edge_editor::make_shared()),
+      pasteboard(pasteboard::make_shared()),
+      database(database::make_shared(project_url->db_file())),
+      exporter(exporter::make_shared()),
+      time_editor_maker(time_editor_maker::make_shared()),
       editor(project_editor::make_shared(identifier, file_info, this->nudging, this->timing)) {
 }
--- audio_editor_core/audio_editor_core/hierarchy/app_level/project_level/project_editor_level/ae_project_editor_level.h
+++ audio_editor_core/audio_editor_core/hierarchy/app_level/project_level/project_editor_level/ae_project_editor_level.h
@@ -11,8 +11,16 @@
 
 namespace yas::ae {
 class file_info;
+class project_url;
 class timing;
 class nudging;
+class file_track;
+class marker_pool;
+class edge_editor;
+class pasteboard;
+class database;
+class exporter;
+class time_editor_maker;
 class project_editor;
 
 struct project_editor_level final {
@@ -24,9 +32,16 @@ struct project_editor_level final {
 
     std::shared_ptr<timing> const timing;
     std::shared_ptr<nudging> const nudging;
+    std::shared_ptr<file_track> const file_track;
+    std::shared_ptr<marker_pool> const marker_pool;
+    std::shared_ptr<edge_editor> const edge_editor;
+    std::shared_ptr<pasteboard> const pasteboard;
+    std::shared_ptr<database> const database;
+    std::shared_ptr<exporter> const exporter;
+    std::shared_ptr<time_editor_maker> const time_editor_maker;
     std::shared_ptr<project_editor> const editor;
 
    private:
-    project_editor_level(std::string const &identifier, ae::file_info const &);
+    project_editor_level(std::string const &identifier, ae::file_info const &, std::shared_ptr<project_url> const &);
 };
 }  // namespace yas::ae
