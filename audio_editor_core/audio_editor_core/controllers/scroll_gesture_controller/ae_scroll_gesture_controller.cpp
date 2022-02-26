088bbeded4850f25d927ab56668cddfecf8e13ec
Yuki Yasoshima <yukiyasos@gmail.com>
2022-02-26 15:06:32 +0900

    scroll_gesture_controller

--- audio_editor_core/audio_editor_core/controllers/scroll_gesture_controller/ae_scroll_gesture_controller.cpp
+++ audio_editor_core/audio_editor_core/controllers/scroll_gesture_controller/ae_scroll_gesture_controller.cpp
@@ -4,18 +4,16 @@
 
 #include "ae_scroll_gesture_controller.h"
 
-#include <audio_editor_core/ae_app_level.h>
 #include <audio_editor_core/ae_gesture.h>
+#include <audio_editor_core/ae_hierarchy.h>
 #include <audio_editor_core/ae_project.h>
-#include <audio_editor_core/ae_project_level.h>
-#include <audio_editor_core/ae_project_level_pool.h>
 #include <audio_editor_core/ae_scrolling.h>
 
 using namespace yas;
 using namespace yas::ae;
 
 std::shared_ptr<scroll_gesture_controller> scroll_gesture_controller::make_shared(std::string const &project_id) {
-    auto const project_level = app_level::global()->project_pool->project_level_for_id(project_id);
+    auto const project_level = hierarchy::project_level_for_id(project_id);
     return std::shared_ptr<scroll_gesture_controller>(new scroll_gesture_controller{project_level->scrolling});
 }
 
