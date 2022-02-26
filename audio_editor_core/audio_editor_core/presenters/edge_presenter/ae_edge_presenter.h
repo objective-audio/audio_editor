17d70eb846a5b1ec744290a2717ac3f1ffbf3850
Yuki Yasoshima <yukiyasos@gmail.com>
2022-02-26 14:16:24 +0900

    edge_presenter

--- audio_editor_core/audio_editor_core/presenters/edge_presenter/ae_edge_presenter.h
+++ audio_editor_core/audio_editor_core/presenters/edge_presenter/ae_edge_presenter.h
@@ -6,6 +6,7 @@
 
 #include <audio_editor_core/ae_edge_location.h>
 #include <audio_editor_core/ae_edge_presenter_dependency.h>
+#include <audio_editor_core/ae_file_info.h>
 #include <observing/yas_observing_umbrella.h>
 
 namespace yas::ae {
@@ -15,7 +16,8 @@ struct edge_presenter final {
     [[nodiscard]] static std::shared_ptr<edge_presenter> make_shared(std::string const &project_id,
                                                                      std::shared_ptr<display_space> const &);
     [[nodiscard]] static std::shared_ptr<edge_presenter> make_shared(
-        std::shared_ptr<project_editor_for_edge_presenter> const &, std::shared_ptr<display_space> const &);
+        file_info const &, std::shared_ptr<project_editor_for_edge_presenter> const &,
+        std::shared_ptr<display_space> const &);
 
     [[nodiscard]] edge_locations locations() const;
     [[nodiscard]] observing::syncable observe_locations(std::function<void(edge_locations const &)> &&);
@@ -23,12 +25,14 @@ struct edge_presenter final {
    private:
     observing::value::holder_ptr<edge_locations> const _locations;
 
+    file_info const _file_info;
     std::weak_ptr<project_editor_for_edge_presenter> const _editor;
     std::shared_ptr<display_space> const _display_space;
 
     observing::canceller_pool _pool;
 
-    edge_presenter(std::shared_ptr<project_editor_for_edge_presenter> const &, std::shared_ptr<display_space> const &);
+    edge_presenter(file_info const &, std::shared_ptr<project_editor_for_edge_presenter> const &,
+                   std::shared_ptr<display_space> const &);
 
     void _update_locations();
 };
