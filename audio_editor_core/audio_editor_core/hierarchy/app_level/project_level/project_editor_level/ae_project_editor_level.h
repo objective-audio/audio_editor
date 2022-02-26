f2c71092b25521ba870d7753cfff4f35ecc06dc7
Yuki Yasoshima <yukiyasos@gmail.com>
2022-02-26 14:12:04 +0900

    project_editor_level file_info

--- audio_editor_core/audio_editor_core/hierarchy/app_level/project_level/project_editor_level/ae_project_editor_level.h
+++ audio_editor_core/audio_editor_core/hierarchy/app_level/project_level/project_editor_level/ae_project_editor_level.h
@@ -4,6 +4,8 @@
 
 #pragma once
 
+#include <audio_editor_core/ae_file_loader_types.h>
+
 #include <memory>
 
 namespace yas::ae {
@@ -16,11 +18,12 @@ struct project_editor_level final {
     [[nodiscard]] static std::shared_ptr<project_editor_level> make_shared(std::string const &identifier,
                                                                            file_info const &);
 
+    file_info const file_info;
     std::shared_ptr<timing> const timing;
     std::shared_ptr<nudging> const nudging;
     std::shared_ptr<project_editor> const editor;
 
    private:
-    project_editor_level(std::string const &identifier, file_info const &);
+    project_editor_level(std::string const &identifier, ae::file_info const &);
 };
 }  // namespace yas::ae
