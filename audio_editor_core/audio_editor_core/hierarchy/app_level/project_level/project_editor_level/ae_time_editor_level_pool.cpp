bc18ffc10d31c5fd10501ed8575e0a1ee0bfcd60
Yuki Yasoshima <yukiyasos@gmail.com>
2022-02-27 17:27:20 +0900

    time_editor_level_pool

--- audio_editor_core/audio_editor_core/hierarchy/app_level/project_level/project_editor_level/ae_time_editor_level_pool.cpp
+++ audio_editor_core/audio_editor_core/hierarchy/app_level/project_level/project_editor_level/ae_time_editor_level_pool.cpp
@@ -4,12 +4,27 @@
 
 #include "ae_time_editor_level_pool.h"
 
+#include <audio_editor_core/ae_time_editor.h>
+#include <audio_editor_core/ae_time_editor_level.h>
+
 using namespace yas;
 using namespace yas::ae;
 
-std::shared_ptr<time_editor_level_pool> time_editor_level_pool::make_shared() {
-    return std::shared_ptr<time_editor_level_pool>(new time_editor_level_pool{});
+std::shared_ptr<time_editor_level_pool> time_editor_level_pool::make_shared(std::string const &identifier) {
+    return std::shared_ptr<time_editor_level_pool>(new time_editor_level_pool{identifier});
 }
 
-time_editor_level_pool::time_editor_level_pool() {
+time_editor_level_pool::time_editor_level_pool(std::string const &identifier) : _identifier(identifier) {
+}
+
+void time_editor_level_pool::add_level(number_components const &components) {
+    if (this->_level != nullptr) {
+        throw std::runtime_error("level is not null.");
+    }
+
+    this->_level = time_editor_level::make_shared(time_editor::make_shared(components));
+}
+
+std::shared_ptr<time_editor_level> const &time_editor_level_pool::level() {
+    return this->_level;
 }
--- audio_editor_core/audio_editor_core/hierarchy/app_level/project_level/project_editor_level/ae_time_editor_level_pool.h
+++ audio_editor_core/audio_editor_core/hierarchy/app_level/project_level/project_editor_level/ae_time_editor_level_pool.h
@@ -5,12 +5,23 @@
 #pragma once
 
 #include <memory>
+#include <string>
 
 namespace yas::ae {
+class time_editor_level;
+class number_components;
+
 struct time_editor_level_pool final {
-    [[nodiscard]] static std::shared_ptr<time_editor_level_pool> make_shared();
+    [[nodiscard]] static std::shared_ptr<time_editor_level_pool> make_shared(std::string const &identifier);
+
+    void add_level(number_components const &);
+    std::shared_ptr<time_editor_level> const &level();
 
    private:
-    time_editor_level_pool();
+    std::string const _identifier;
+
+    std::shared_ptr<time_editor_level> _level;
+
+    time_editor_level_pool(std::string const &identifier);
 };
 }  // namespace yas::ae
