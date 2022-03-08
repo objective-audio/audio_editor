21e6793925a736a7bbfe5656a33e493ac7162498
Yuki Yasoshima <yukiyasos@gmail.com>
2022-03-08 12:44:27 +0900

    begin_time_editing

--- audio_editor_core/audio_editor_core/hierarchy/app_level/project_level/project_editor_level/ae_time_editor_level_pool.h
+++ audio_editor_core/audio_editor_core/hierarchy/app_level/project_level/project_editor_level/ae_time_editor_level_pool.h
@@ -16,7 +16,7 @@ class number_components;
 struct time_editor_level_pool final {
     [[nodiscard]] static std::shared_ptr<time_editor_level_pool> make_shared(std::string const &identifier);
 
-    void add_level(number_components const &);
+    void add_level(number_components const &, std::optional<std::size_t> const unit_idx);
     void remove_level();
 
     [[nodiscard]] std::shared_ptr<time_editor_level> const &level();
--- audio_editor_core/audio_editor_core/hierarchy/app_level/project_level/project_editor_level/time_editor_level/ae_time_editor_level.cpp
+++ audio_editor_core/audio_editor_core/hierarchy/app_level/project_level/project_editor_level/time_editor_level/ae_time_editor_level.cpp
@@ -9,10 +9,11 @@
 using namespace yas;
 using namespace yas::ae;
 
-std::shared_ptr<time_editor_level> time_editor_level::make_shared(number_components const &components) {
-    return std::shared_ptr<time_editor_level>(new time_editor_level{components});
+std::shared_ptr<time_editor_level> time_editor_level::make_shared(number_components const &components,
+                                                                  std::optional<std::size_t> const unit_idx) {
+    return std::shared_ptr<time_editor_level>(new time_editor_level{components, unit_idx});
 }
 
-time_editor_level::time_editor_level(number_components const &components)
-    : time_editor(time_editor::make_shared(components)) {
+time_editor_level::time_editor_level(number_components const &components, std::optional<std::size_t> const unit_idx)
+    : time_editor(time_editor::make_shared(components, unit_idx)) {
 }
