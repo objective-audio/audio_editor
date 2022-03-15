e59b81403ca4c02dccd1662516b280be66ae369d
Yuki Yasoshima <yukiyasos@gmail.com>
2022-03-15 21:13:53 +0900

    time_editor_level project_id

--- audio_editor_core/audio_editor_core/hierarchy/app_level/project_level/project_editor_level/time_editor_level/ae_time_editor_level.h
+++ audio_editor_core/audio_editor_core/hierarchy/app_level/project_level/project_editor_level/time_editor_level/ae_time_editor_level.h
@@ -6,18 +6,22 @@
 
 #include <memory>
 #include <optional>
+#include <string>
 
 namespace yas::ae {
 class number_components;
 class time_editor;
 
 struct time_editor_level final {
-    [[nodiscard]] static std::shared_ptr<time_editor_level> make_shared(number_components const &,
+    [[nodiscard]] static std::shared_ptr<time_editor_level> make_shared(std::string const &project_id,
+                                                                        number_components const &,
                                                                         std::optional<std::size_t> const unit_idx);
 
+    std::string const project_id;
     std::shared_ptr<time_editor> const time_editor;
 
    private:
-    time_editor_level(number_components const &, std::optional<std::size_t> const unit_idx);
+    time_editor_level(std::string const &project_id, number_components const &,
+                      std::optional<std::size_t> const unit_idx);
 };
 }  // namespace yas::ae
