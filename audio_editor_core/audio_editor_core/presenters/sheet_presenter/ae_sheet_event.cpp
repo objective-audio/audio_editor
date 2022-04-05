81b0bee420cb11f0dd8b0e22174b834e27ab1de6
Yuki Yasoshima <yukiyasos@gmail.com>
2022-04-05 21:29:28 +0900

    add sheet_presenter

---
+++ audio_editor_core/audio_editor_core/presenters/sheet_presenter/ae_sheet_event.cpp
@@ -0,0 +1,8 @@
+//
+//  ae_sheet_event.cpp
+//
+
+#include "ae_sheet_event.h"
+
+using namespace yas;
+using namespace yas::ae;
---
+++ audio_editor_core/audio_editor_core/presenters/sheet_presenter/ae_sheet_presenter.cpp
@@ -0,0 +1,23 @@
+//
+//  ae_sheet_presenter.cpp
+//
+
+#include "ae_sheet_presenter.h"
+
+using namespace yas;
+using namespace yas::ae;
+
+std::shared_ptr<sheet_presenter> sheet_presenter::make_shared() {
+    return std::shared_ptr<sheet_presenter>(new sheet_presenter{});
+}
+
+sheet_presenter::sheet_presenter() : _event_notifier(observing::notifier<sheet_event>::make_shared()) {
+}
+
+void sheet_presenter::notify_event(sheet_event const &event) {
+    this->_event_notifier->notify(event);
+}
+
+observing::endable sheet_presenter::observe_event(std::function<void(sheet_event const &)> &&handler) {
+    return this->_event_notifier->observe(std::move(handler));
+}
