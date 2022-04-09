f863e1cb24d51f01335763f08ce27baf8cda4ea4
Yuki Yasoshima <yukiyasos@gmail.com>
2022-04-09 12:42:15 +0900

    module name view

---
+++ audio_editor_core/audio_editor_core/presenters/module_name_presenter/ae_module_name_presenter.h
@@ -0,0 +1,28 @@
+//
+//  ae_module_name_presenter.h
+//
+
+#pragma once
+
+#include <audio_editor_core/ae_common_types.h>
+
+#include <memory>
+#include <string>
+
+namespace yas::ae {
+class file_track;
+
+struct module_name_presenter final {
+    [[nodiscard]] static std::shared_ptr<module_name_presenter> make_shared(std::string const &project_id,
+                                                                            time::range const module_range);
+
+    std::string const &name() const;
+    void set_name(std::string const &);
+
+   private:
+    time::range const _module_range;
+    std::weak_ptr<file_track> const _file_track;
+
+    module_name_presenter(time::range const &, std::shared_ptr<file_track> const &);
+};
+}  // namespace yas::ae
