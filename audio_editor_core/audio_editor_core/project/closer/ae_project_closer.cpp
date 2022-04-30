0811ac7a882a0a2f110c7b19628c3d824b0745de
Yuki Yasoshima <yukiyasos@gmail.com>
2022-04-30 22:52:36 +0900

    implement project_closer

--- audio_editor_core/audio_editor_core/project/closer/ae_project_closer.cpp
+++ audio_editor_core/audio_editor_core/project/closer/ae_project_closer.cpp
@@ -4,12 +4,55 @@
 
 #include "ae_project_closer.h"
 
+#include <audio_editor_core/ae_file_importer.h>
+#include <audio_editor_core/ae_project_editor_level_pool.h>
+#include <audio_editor_core/ae_project_status.h>
+
 using namespace yas;
 using namespace yas::ae;
 
-std::shared_ptr<project_closer> project_closer::make_shared() {
-    return std::shared_ptr<project_closer>(new project_closer{});
+std::shared_ptr<project_closer> project_closer::make_shared(
+    std::string const &project_id, std::shared_ptr<file_importer> const &file_importer,
+    std::shared_ptr<project_editor_level_pool> const &editor_level_pool,
+    std::shared_ptr<project_status> const &status) {
+    return std::shared_ptr<project_closer>(new project_closer{project_id, file_importer, editor_level_pool, status});
 }
 
-project_closer::project_closer() {
+project_closer::project_closer(std::string const &project_id, std::shared_ptr<file_importer> const &file_importer,
+                               std::shared_ptr<project_editor_level_pool> const &editor_level_pool,
+                               std::shared_ptr<project_status> const &status)
+    : _project_id(project_id),
+      _file_importer(file_importer),
+      _editor_level_pool(editor_level_pool),
+      _status(status),
+      _event_notifier(observing::notifier<project_event>::make_shared()) {
+}
+
+bool project_closer::can_close() const {
+    return true;
+}
+
+void project_closer::request_close() {
+    switch (this->_status->state()) {
+        case project_state::closing:
+            return;
+        case project_state::loading:
+            this->_file_importer->cancel(this->_project_id);
+            this->_status->set_state(project_state::closing);
+            break;
+        case project_state::editing:
+            this->_editor_level_pool->remove_level();
+            this->_status->set_state(project_state::closing);
+            break;
+        case project_state::launching:
+        case project_state::failure:
+            this->_status->set_state(project_state::closing);
+            break;
+    }
+
+    this->_event_notifier->notify(project_event::should_close);
+}
+
+observing::endable project_closer::observe_event(std::function<void(project_event const &)> &&handler) {
+    return this->_event_notifier->observe(std::move(handler));
 }
--- audio_editor_core/audio_editor_core/project/closer/ae_project_closer.h
+++ audio_editor_core/audio_editor_core/project/closer/ae_project_closer.h
@@ -4,13 +4,35 @@
 
 #pragma once
 
-#include <memory>
+#include <audio_editor_core/ae_project_types.h>
+#include <observing/yas_observing_umbrella.h>
 
 namespace yas::ae {
+class file_importer;
+class project_editor_level_pool;
+class project_status;
+
 struct project_closer final {
-    [[nodiscard]] static std::shared_ptr<project_closer> make_shared();
+    [[nodiscard]] static std::shared_ptr<project_closer> make_shared(std::string const &project_id,
+                                                                     std::shared_ptr<file_importer> const &,
+                                                                     std::shared_ptr<project_editor_level_pool> const &,
+                                                                     std::shared_ptr<project_status> const &);
+
+    [[nodiscard]] bool can_close() const;
+    void request_close();
+
+    [[nodiscard]] observing::endable observe_event(std::function<void(project_event const &)> &&);
 
    private:
-    project_closer();
+    std::string const _project_id;
+
+    std::shared_ptr<file_importer> const _file_importer;
+    std::shared_ptr<project_editor_level_pool> const _editor_level_pool;
+    std::shared_ptr<project_status> const _status;
+
+    observing::notifier_ptr<project_event> const _event_notifier;
+
+    project_closer(std::string const &project_id, std::shared_ptr<file_importer> const &,
+                   std::shared_ptr<project_editor_level_pool> const &, std::shared_ptr<project_status> const &);
 };
 }  // namespace yas::ae
