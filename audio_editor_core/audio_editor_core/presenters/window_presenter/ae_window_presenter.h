88589a0526140224eba8012fd0b7021f80e118fc
Yuki Yasoshima <yukiyasos@gmail.com>
2022-04-30 23:20:12 +0900

    window_presenter

--- audio_editor_core/audio_editor_core/presenters/window_presenter/ae_window_presenter.h
+++ audio_editor_core/audio_editor_core/presenters/window_presenter/ae_window_presenter.h
@@ -10,7 +10,7 @@ namespace yas::ae {
 struct window_presenter final {
     static std::shared_ptr<window_presenter> make_shared(std::string const &project_id);
     static std::shared_ptr<window_presenter> make_shared(std::string const &project_id, url const &file_url,
-                                                         std::shared_ptr<project_for_window_presenter> const &);
+                                                         std::shared_ptr<project_closer_for_window_presenter> const &);
 
     std::string const project_id;
 
@@ -20,10 +20,10 @@ struct window_presenter final {
 
    private:
     url const _file_url;
-    std::shared_ptr<project_for_window_presenter> _project;
+    std::shared_ptr<project_closer_for_window_presenter> _closer;
 
     window_presenter(std::string const &project_id, url const &file_url,
-                     std::shared_ptr<project_for_window_presenter> const &);
+                     std::shared_ptr<project_closer_for_window_presenter> const &);
 
     window_presenter(window_presenter const &) = delete;
     window_presenter(window_presenter &&) = delete;
