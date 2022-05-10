fe2f7147b86de19bb8fa9abf19b6ffb39a9ded6e
Yuki Yasoshima <yukiyasos@gmail.com>
2022-05-10 23:13:39 +0900

    app_level_router

---
+++ audio_editor_core/audio_editor_core/levels/ae_app_level_router.cpp
@@ -0,0 +1,30 @@
+//
+//  ae_app_level_router.cpp
+//
+
+#include "ae_app_level_router.h"
+
+#include <audio_editor_core/ae_app_level.h>
+#include <cpp_utils/yas_assertion.h>
+
+using namespace yas;
+using namespace yas::ae;
+
+std::shared_ptr<app_level_router> app_level_router::make_shared() {
+    return std::make_shared<app_level_router>();
+}
+
+app_level_router::app_level_router() {
+}
+
+std::shared_ptr<app_level> const &app_level_router::level() const {
+    return this->_level;
+}
+
+void app_level_router::add_level() {
+    if (this->_level) {
+        assertion_failure_if_not_test();
+        return;
+    }
+    this->_level = app_level::make_shared();
+}
