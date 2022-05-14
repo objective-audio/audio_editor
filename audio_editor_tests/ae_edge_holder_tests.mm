1efe08e6dd9229b27ed9478df261c1b49a460cdf
Yuki Yasoshima <yukiyasos@gmail.com>
2022-05-14 11:30:27 +0900

    rename holder

--- audio_editor_tests/ae_edge_holder_tests.mm
+++ audio_editor_tests/ae_edge_holder_tests.mm
@@ -21,57 +21,57 @@ using namespace yas::ae;
 }
 
 - (void)test_set_edge {
-    auto const editor = edge_holder::make_shared();
+    auto const holder = edge_holder::make_shared();
 
     std::vector<edge_holder_event> called;
 
     auto const canceller =
-        editor->observe_event([&called](edge_holder_event const &event) { called.push_back(event); }).sync();
+        holder->observe_event([&called](edge_holder_event const &event) { called.push_back(event); }).sync();
 
     XCTAssertEqual(called.size(), 1);
     XCTAssertEqual(called.at(0).type, edge_holder_event_type::fetched);
     XCTAssertEqual(called.at(0).edge, ae::edge::zero());
 
-    editor->set_edge(ae::edge::zero());
+    holder->set_edge(ae::edge::zero());
 
     XCTAssertEqual(called.size(), 1);
 
-    editor->set_edge({.begin_frame = 1, .end_frame = 2});
+    holder->set_edge({.begin_frame = 1, .end_frame = 2});
 
     XCTAssertEqual(called.size(), 2);
     XCTAssertEqual(called.at(1).type, edge_holder_event_type::updated);
     XCTAssertEqual(called.at(1).edge, (ae::edge{.begin_frame = 1, .end_frame = 2}));
 
-    XCTAssertThrows(editor->set_edge({.begin_frame = 1, .end_frame = 0}));
+    XCTAssertThrows(holder->set_edge({.begin_frame = 1, .end_frame = 0}));
 
     canceller->cancel();
 }
 
 - (void)test_set_begin_frame {
-    auto const editor = edge_holder::make_shared();
-    editor->set_edge({.begin_frame = 0, .end_frame = 2});
+    auto const holder = edge_holder::make_shared();
+    holder->set_edge({.begin_frame = 0, .end_frame = 2});
 
     std::vector<edge_holder_event> called;
 
     auto const canceller =
-        editor->observe_event([&called](edge_holder_event const &event) { called.push_back(event); }).sync();
+        holder->observe_event([&called](edge_holder_event const &event) { called.push_back(event); }).sync();
 
     XCTAssertEqual(called.size(), 1);
 
-    editor->set_begin_frame(0);
+    holder->set_begin_frame(0);
 
     XCTAssertEqual(called.size(), 1);
 
-    editor->set_begin_frame(1);
+    holder->set_begin_frame(1);
 
-    XCTAssertEqual(editor->edge(), (ae::edge{.begin_frame = 1, .end_frame = 2}));
+    XCTAssertEqual(holder->edge(), (ae::edge{.begin_frame = 1, .end_frame = 2}));
     XCTAssertEqual(called.size(), 2);
     XCTAssertEqual(called.at(1).type, edge_holder_event_type::updated);
     XCTAssertEqual(called.at(1).edge, (ae::edge{.begin_frame = 1, .end_frame = 2}));
 
-    editor->set_begin_frame(3);
+    holder->set_begin_frame(3);
 
-    XCTAssertEqual(editor->edge(), (ae::edge{.begin_frame = 3, .end_frame = 3}));
+    XCTAssertEqual(holder->edge(), (ae::edge{.begin_frame = 3, .end_frame = 3}));
     XCTAssertEqual(called.size(), 3);
     XCTAssertEqual(called.at(2).type, edge_holder_event_type::updated);
     XCTAssertEqual(called.at(2).edge, (ae::edge{.begin_frame = 3, .end_frame = 3}));
@@ -80,30 +80,30 @@ using namespace yas::ae;
 }
 
 - (void)test_set_end_frame {
-    auto const editor = edge_holder::make_shared();
-    editor->set_edge({.begin_frame = 1, .end_frame = 2});
+    auto const holder = edge_holder::make_shared();
+    holder->set_edge({.begin_frame = 1, .end_frame = 2});
 
     std::vector<edge_holder_event> called;
 
     auto const canceller =
-        editor->observe_event([&called](edge_holder_event const &event) { called.push_back(event); }).sync();
+        holder->observe_event([&called](edge_holder_event const &event) { called.push_back(event); }).sync();
 
     XCTAssertEqual(called.size(), 1);
 
-    editor->set_end_frame(2);
+    holder->set_end_frame(2);
 
     XCTAssertEqual(called.size(), 1);
 
-    editor->set_end_frame(3);
+    holder->set_end_frame(3);
 
-    XCTAssertEqual(editor->edge(), (ae::edge{.begin_frame = 1, .end_frame = 3}));
+    XCTAssertEqual(holder->edge(), (ae::edge{.begin_frame = 1, .end_frame = 3}));
     XCTAssertEqual(called.size(), 2);
     XCTAssertEqual(called.at(1).type, edge_holder_event_type::updated);
     XCTAssertEqual(called.at(1).edge, (ae::edge{.begin_frame = 1, .end_frame = 3}));
 
-    editor->set_end_frame(0);
+    holder->set_end_frame(0);
 
-    XCTAssertEqual(editor->edge(), (ae::edge{.begin_frame = 0, .end_frame = 0}));
+    XCTAssertEqual(holder->edge(), (ae::edge{.begin_frame = 0, .end_frame = 0}));
     XCTAssertEqual(called.size(), 3);
     XCTAssertEqual(called.at(2).type, edge_holder_event_type::updated);
     XCTAssertEqual(called.at(2).edge, (ae::edge{.begin_frame = 0, .end_frame = 0}));
@@ -112,23 +112,23 @@ using namespace yas::ae;
 }
 
 - (void)test_revert_edge {
-    auto const editor = edge_holder::make_shared();
-    editor->set_edge({.begin_frame = 0, .end_frame = 2});
+    auto const holder = edge_holder::make_shared();
+    holder->set_edge({.begin_frame = 0, .end_frame = 2});
 
     std::vector<edge_holder_event> called;
 
     auto const canceller =
-        editor->observe_event([&called](edge_holder_event const &event) { called.push_back(event); }).sync();
+        holder->observe_event([&called](edge_holder_event const &event) { called.push_back(event); }).sync();
 
     XCTAssertEqual(called.size(), 1);
 
-    editor->revert_edge({.begin_frame = 0, .end_frame = 2});
+    holder->revert_edge({.begin_frame = 0, .end_frame = 2});
 
     XCTAssertEqual(called.size(), 1);
 
-    editor->revert_edge({.begin_frame = 1, .end_frame = 3});
+    holder->revert_edge({.begin_frame = 1, .end_frame = 3});
 
-    XCTAssertEqual(editor->edge(), (ae::edge{.begin_frame = 1, .end_frame = 3}));
+    XCTAssertEqual(holder->edge(), (ae::edge{.begin_frame = 1, .end_frame = 3}));
     XCTAssertEqual(called.size(), 2);
     XCTAssertEqual(called.at(1).type, edge_holder_event_type::reverted);
     XCTAssertEqual(called.at(1).edge, (ae::edge{.begin_frame = 1, .end_frame = 3}));
