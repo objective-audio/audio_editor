6eb3ab2cea02aae5b8462327d02c5fdce85c9ba4
Yuki Yasoshima <yukiyasos@gmail.com>
2022-05-15 13:34:45 +0900

    scroller_presenter

--- audio_editor_core/audio_editor_core/presenters/scroller_presenter/ae_scroller_presenter.cpp
+++ audio_editor_core/audio_editor_core/presenters/scroller_presenter/ae_scroller_presenter.cpp
@@ -13,19 +13,18 @@ using namespace yas::ae;
 
 std::shared_ptr<scroller_presenter> scroller_presenter::make_shared(project_id const &project_id) {
     auto const &project_level = hierarchy::project_level_for_id(project_id);
-    auto const &editor_level = hierarchy::project_editor_level_for_id(project_id);
     return std::shared_ptr<scroller_presenter>(
-        new scroller_presenter{editor_level->file_info, project_level->player, project_level->zooming_pair});
+        new scroller_presenter{project_level->project_format, project_level->player, project_level->zooming_pair});
 }
 
-scroller_presenter::scroller_presenter(file_info const &file_info, std::shared_ptr<player> const &player,
+scroller_presenter::scroller_presenter(project_format const &project_format, std::shared_ptr<player> const &player,
                                        std::shared_ptr<zooming_pair> const &zooming_pair)
-    : _file_info(file_info), _player(player), _zooming_pair(zooming_pair) {
+    : _project_format(project_format), _player(player), _zooming_pair(zooming_pair) {
 }
 
 float scroller_presenter::current_position() const {
     if (auto const player = this->_player.lock()) {
-        double const sample_rate = this->_file_info.sample_rate;
+        double const sample_rate = this->_project_format.sample_rate;
         return static_cast<double>(player->current_frame()) / sample_rate;
     }
     return 0.0;
