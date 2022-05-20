e28556562ce3196a941810bbecf0f310a9ea604e
Yuki Yasoshima <yukiyasos@gmail.com>
2022-05-20 21:55:15 +0900

    dialog_presenter

--- audio_editor_core/audio_editor_core/presenters/dialog_presenter/ae_dialog_presenter.h
+++ audio_editor_core/audio_editor_core/presenters/dialog_presenter/ae_dialog_presenter.h
@@ -11,13 +11,13 @@ namespace yas::ae {
 struct dialog_presenter final {
     [[nodiscard]] static std::shared_ptr<dialog_presenter> make_shared();
 
+    dialog_presenter();
+
     void notify_event(dialog_event const &);
 
     observing::endable observe_event(std::function<void(dialog_event const &)> &&);
 
    private:
     observing::notifier_ptr<dialog_event> const _event_notifier;
-
-    dialog_presenter();
 };
 }  // namespace yas::ae
