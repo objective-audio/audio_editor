8227dc0b98f3635e49e9d3d7273fb3b3671ee3a5
Yuki Yasoshima <yukiyasos@gmail.com>
2022-05-21 13:40:50 +0900

    fix

--- audio_editor/audio_editor/AEMetalViewController.mm
+++ audio_editor/audio_editor/AEMetalViewController.mm
@@ -5,7 +5,6 @@
 #import "AEMetalViewController.h"
 #import <UniformTypeIdentifiers/UTCoreTypes.h>
 #include <audio_editor_core/ae_action_controller.h>
-#include <audio_editor_core/ae_dialog_presenter.h>
 #include <audio_editor_core/ae_project_sub_level_router.h>
 #include <audio_editor_core/ae_ui_hierarchy.h>
 #include <audio_editor_core/ae_ui_root_level.h>
