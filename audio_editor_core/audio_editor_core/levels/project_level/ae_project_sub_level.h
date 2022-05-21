0c25f41d13a2d36835817f1c4073c3c39bfea1c4
Yuki Yasoshima <yukiyasos@gmail.com>
2022-05-21 13:20:42 +0900

    project_sub_level

--- audio_editor_core/audio_editor_core/levels/project_level/ae_project_sub_level.h
+++ audio_editor_core/audio_editor_core/levels/project_level/ae_project_sub_level.h
@@ -4,11 +4,13 @@
 
 #pragma once
 
+#include <audio_editor_core/ae_dialog_level.h>
 #include <audio_editor_core/ae_sheet_level.h>
 #include <audio_editor_core/ae_time_editor_level.h>
 
 #include <variant>
 
 namespace yas::ae {
-using project_sub_level = std::variant<std::shared_ptr<time_editor_level>, std::shared_ptr<sheet_level>>;
+using project_sub_level =
+    std::variant<std::shared_ptr<time_editor_level>, std::shared_ptr<sheet_level>, std::shared_ptr<dialog_level>>;
 }  // namespace yas::ae
