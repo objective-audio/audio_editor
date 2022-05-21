013a8f80d229187e961d94a3b66a05597ba2e96e
Yuki Yasoshima <yukiyasos@gmail.com>
2022-05-21 13:43:24 +0900

    export_interactor

--- audio_editor_core/audio_editor_core/interactors/export_interactor/ae_export_interactor.cpp
+++ audio_editor_core/audio_editor_core/interactors/export_interactor/ae_export_interactor.cpp
@@ -4,29 +4,29 @@
 
 #include "ae_export_interactor.h"
 
-#include <audio_editor_core/ae_dialog_presenter.h>
 #include <audio_editor_core/ae_edge_holder.h>
 #include <audio_editor_core/ae_editing_status.h>
 #include <audio_editor_core/ae_exporter.h>
 #include <audio_editor_core/ae_hierarchy.h>
 #include <audio_editor_core/ae_player.h>
+#include <audio_editor_core/ae_project_sub_level_router.h>
 #include <audio_editor_core/ae_timeline_holder.h>
 
 using namespace yas;
 using namespace yas::ae;
 
 std::shared_ptr<export_interactor> export_interactor::make_shared(
-    project_format const &project_format, dialog_presenter *dialog_presenter, editing_status const *editing_status,
+    project_format const &project_format, project_sub_level_router *router, editing_status const *editing_status,
     edge_holder const *edge_holder, player *player, exporter *exporter, timeline_holder const *timeline_holder) {
-    return std::make_shared<export_interactor>(project_format, dialog_presenter, editing_status, edge_holder, player,
-                                               exporter, timeline_holder);
+    return std::make_shared<export_interactor>(project_format, router, editing_status, edge_holder, player, exporter,
+                                               timeline_holder);
 }
 
-export_interactor::export_interactor(project_format const &project_format, dialog_presenter *dialog_presenter,
+export_interactor::export_interactor(project_format const &project_format, project_sub_level_router *router,
                                      editing_status const *editing_status, edge_holder const *edge_holder,
                                      player *player, exporter *exporter, timeline_holder const *timeline_holder)
     : _project_format(project_format),
-      _dialog_presenter(dialog_presenter),
+      _router(router),
       _editing_status(editing_status),
       _edge_holder(edge_holder),
       _player(player),
@@ -43,11 +43,11 @@ void export_interactor::select_file_for_export() {
         return;
     }
 
-    this->_dialog_presenter->notify_event(dialog_event::select_file_for_export);
+    this->_router->add_dialog(dialog_content::select_file_for_export);
 }
 
 bool export_interactor::can_export_to_file() const {
-    if (!this->_editing_status->can_editing()) {
+    if (!this->_editing_status->can_editing() || this->_router->sub_level().has_value()) {
         return false;
     }
 
