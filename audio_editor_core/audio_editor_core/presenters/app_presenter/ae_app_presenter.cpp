ae64d7d9b15e1a2554991cb8748552251c8a7b50
Yuki Yasoshima <yukiyasos@gmail.com>
2022-05-24 20:10:54 +0900

    presenter

--- audio_editor_core/audio_editor_core/presenters/app_presenter/ae_app_presenter.cpp
+++ audio_editor_core/audio_editor_core/presenters/app_presenter/ae_app_presenter.cpp
@@ -4,6 +4,8 @@
 
 #include "ae_app_presenter.h"
 
+#include <audio_editor_core/ae_app_dialog_level.h>
+#include <audio_editor_core/ae_app_dialog_level_router.h>
 #include <audio_editor_core/ae_app_level.h>
 #include <audio_editor_core/ae_app_presenter_utils.h>
 #include <audio_editor_core/ae_hierarchy.h>
@@ -15,12 +17,14 @@ using namespace yas::ae;
 
 std::shared_ptr<app_presenter> app_presenter::make_shared() {
     return std::make_shared<app_presenter>(hierarchy::app_level()->project_level_router,
+                                           hierarchy::app_level()->dialog_level_router,
                                            hierarchy::app_level()->project_preparer);
 }
 
 app_presenter::app_presenter(std::shared_ptr<project_level_router_for_app_presenter> const &pool,
+                             std::shared_ptr<app_dialog_level_router> const &dialog_level_router,
                              std::shared_ptr<project_preparer> const &preparer)
-    : _project_pool(pool), _project_preparer(preparer) {
+    : _project_level_router(pool), _dialog_level_router(dialog_level_router), _project_preparer(preparer) {
 }
 
 bool app_presenter::can_open_file_dialog() const {
@@ -40,7 +44,7 @@ void app_presenter::select_file(url const &file_url) {
 }
 
 observing::syncable app_presenter::observe_event(std::function<void(app_presenter_event const &)> &&handler) {
-    if (auto const pool = this->_project_pool.lock()) {
+    if (auto const pool = this->_project_level_router.lock()) {
         auto syncable = pool->observe_event([handler](project_level_router_event const &pool_event) {
             handler(app_presenter_event{.type = to_presenter_event_type(pool_event.type),
                                         .project_id = pool_event.project_id});
@@ -49,6 +53,21 @@ observing::syncable app_presenter::observe_event(std::function<void(app_presente
         syncable.merge(this->_event_notifier->observe([handler](auto const &event) { handler(event); }));
 
         return syncable;
+    } else {
+        return observing::syncable{};
+    }
+}
+
+observing::syncable app_presenter::observe_dialog(std::function<void(std::optional<app_dialog_content>)> &&handler) {
+    if (auto const router = this->_dialog_level_router.lock()) {
+        return router->observe(
+            [handler = std::move(handler)](std::optional<std::shared_ptr<app_dialog_level>> const &level) {
+                if (level.has_value()) {
+                    handler(level.value()->content);
+                } else {
+                    handler(std::nullopt);
+                }
+            });
     } else {
         return observing::syncable{};
     }
--- audio_editor_core/audio_editor_core/presenters/app_presenter/ae_app_presenter.h
+++ audio_editor_core/audio_editor_core/presenters/app_presenter/ae_app_presenter.h
@@ -4,26 +4,30 @@
 
 #pragma once
 
+#include <audio_editor_core/ae_app_dialog_content.h>
 #include <audio_editor_core/ae_app_presenter_dependency.h>
 #include <audio_editor_core/ae_app_presenter_types.h>
 
 namespace yas::ae {
 class project_preparer;
+class app_dialog_level_router;
 
 struct app_presenter final {
     [[nodiscard]] static std::shared_ptr<app_presenter> make_shared();
 
     app_presenter(std::shared_ptr<project_level_router_for_app_presenter> const &,
-                  std::shared_ptr<project_preparer> const &);
+                  std::shared_ptr<app_dialog_level_router> const &, std::shared_ptr<project_preparer> const &);
 
     [[nodiscard]] bool can_open_file_dialog() const;
     void open_file_dialog();
     void select_file(url const &);
 
     [[nodiscard]] observing::syncable observe_event(std::function<void(app_presenter_event const &)> &&);
+    [[nodiscard]] observing::syncable observe_dialog(std::function<void(std::optional<app_dialog_content>)> &&);
 
    private:
-    std::weak_ptr<project_level_router_for_app_presenter> _project_pool;
+    std::weak_ptr<project_level_router_for_app_presenter> const _project_level_router;
+    std::weak_ptr<app_dialog_level_router> const _dialog_level_router;
     std::weak_ptr<project_preparer> const _project_preparer;
     observing::notifier_ptr<app_presenter_event> const _event_notifier =
         observing::notifier<app_presenter_event>::make_shared();
