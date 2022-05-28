3d3684def869810d88245cc0e92a7c92aef6ea5d
Yuki Yasoshima <yukiyasos@gmail.com>
2022-05-28 23:06:44 +0900

    project_setup

--- audio_editor_core/audio_editor_core/interactors/project_setup/ae_project_setup.cpp
+++ audio_editor_core/audio_editor_core/interactors/project_setup/ae_project_setup.cpp
@@ -16,27 +16,27 @@ std::shared_ptr<project_setup> project_setup::make_shared(project_format_setup *
     auto const &app_level = hierarchy::app_level();
     auto const &preparer = app_level->project_preparer;
     auto const &router = app_level->dialog_level_router;
-    return std::make_shared<project_setup>(format_setup, preparer, router);
+    return std::make_shared<project_setup>(format_setup, preparer.get(), router.get());
 }
 
-project_setup::project_setup(project_format_setup *format_setup, std::shared_ptr<project_preparer> const &preparer,
-                             std::shared_ptr<app_dialog_sub_level_router> const &router)
+project_setup::project_setup(project_format_setup *format_setup, project_preparer *preparer,
+                             app_dialog_sub_level_router *router)
     : _format_setup(format_setup), _preparer(preparer), _router(router) {
 }
 
 void project_setup::select_directory(url const &file_url) {
-    if (auto const preparer = this->_preparer.lock()) {
-        preparer->prepare(this->_format_setup->format(), file_url);
+    if (this->_preparer) {
+        this->_preparer->prepare(this->_format_setup->format(), file_url);
     }
 
     this->finalize();
 }
 
 void project_setup::finalize() {
-    if (auto const router = this->_router.lock()) {
-        router->remove_project_format_dialog();
+    if (this->_router) {
+        this->_router->remove_project_format_dialog();
     }
 
-    this->_router.reset();
-    this->_preparer.reset();
+    this->_router = nullptr;
+    this->_preparer = nullptr;
 }
