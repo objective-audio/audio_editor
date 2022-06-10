810c3d8d8dde968ad35bf957448be13c5c2dafc0
Yuki Yasoshima <yukiyasos@gmail.com>
2022-06-10 20:21:11 +0900

    fix tests

--- audio_editor_tests/ae_pasting_file_module_tests.mm
+++ audio_editor_tests/ae_pasting_file_module_tests.mm
@@ -16,31 +16,35 @@ using namespace yas::ae;
 @implementation ae_pasting_file_module_tests
 
 - (void)test_data {
-    pasting_file_module const module{.file_frame = 1, .length = 2, .file_name = "test-name-1"};
+    pasting_file_module const module{
+        .name = "test-name-1", .file_frame = 1, .length = 2, .file_name = "test-file-name-1"};
 
-    XCTAssertEqual(module.data(), "file_frame=1,file_name=test-name-1,kind=file_module,length=2");
+    XCTAssertEqual(module.data(), "file_frame=1,file_name=test-file-name-1,kind=file_module,length=2,name=test-name-1");
 }
 
 - (void)test_make {
-    auto const module =
-        pasting_file_module::make_value("file_frame=10,file_name=test-name-2,kind=file_module,length=20");
+    auto const module = pasting_file_module::make_value(
+        "file_frame=10,file_name=test-file-name-2,kind=file_module,length=20,name=test-name-2");
 
     XCTAssertTrue(module.has_value());
+    XCTAssertEqual(module.value().name, "test-name-2");
     XCTAssertEqual(module.value().file_frame, 10);
     XCTAssertEqual(module.value().length, 20);
-    XCTAssertEqual(module.value().file_name, "test-name-2");
+    XCTAssertEqual(module.value().file_name, "test-file-name-2");
 
     XCTAssertFalse(pasting_file_module::make_value("").has_value());
 }
 
 - (void)test_to_string {
-    pasting_file_module const module{.file_frame = 100, .length = 200, .file_name = "test-name-3"};
+    pasting_file_module const module{
+        .name = "test-name-3", .file_frame = 100, .length = 200, .file_name = "test-file-name-3"};
 
-    XCTAssertEqual(to_string(module), "{file_frame:100, length:200, file_name:test-name-3}");
+    XCTAssertEqual(to_string(module), "{name:test-name-3, file_frame:100, length:200, file_name:test-file-name-3}");
 }
 
 - (void)test_ostream {
-    pasting_file_module const value{.file_frame = 1000, .length = 2000, .file_name = "test-name-4"};
+    pasting_file_module const value{
+        .name = "test-name-4", .file_frame = 1000, .length = 2000, .file_name = "test-file-name-4"};
 
     std::ostringstream stream;
     stream << value;
