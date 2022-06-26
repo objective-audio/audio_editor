448a3c8323805209421a9ca43e7d609948a3d90a
Yuki Yasoshima <yukiyasos@gmail.com>
2022-06-26 14:52:58 +0900

    fix tests

--- audio_editor_tests/utils/ae_test_utils.h
+++ audio_editor_tests/utils/ae_test_utils.h
@@ -4,10 +4,16 @@
 
 #pragma once
 
+#include <audio_editor_core/ae_action.h>
 #include <cpp_utils/yas_url.h>
+
+#include <optional>
 
 namespace yas::ae::test_utils {
 url test_url();
 void create_test_directory();
 void remove_contents_in_test_directory();
 }  // namespace yas::ae::test_utils
+
+bool operator==(yas::ae::action const &lhs, yas::ae::action const &rhs);
+bool operator==(std::optional<yas::ae::action> const &lhs, std::optional<yas::ae::action> const &rhs);
