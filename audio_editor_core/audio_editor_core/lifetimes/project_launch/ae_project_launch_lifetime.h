0760c25165d52534aeb130337ad38763c8b3fe27
Yuki Yasoshima <yukiyasos@gmail.com>
2022-07-09 22:58:15 +0900

    launcher

--- audio_editor_core/audio_editor_core/lifetimes/project_launch/ae_project_launch_lifetime.h
+++ audio_editor_core/audio_editor_core/lifetimes/project_launch/ae_project_launch_lifetime.h
@@ -9,11 +9,16 @@
 #include <memory>
 
 namespace yas::ae {
+class project_launcher;
+class window_lifetime;
+
 struct project_launch_lifetime final {
     [[nodiscard]] static std::shared_ptr<project_launch_lifetime> make_shared(window_lifetime_id const &);
 
-    project_launch_lifetime(window_lifetime_id const &);
+    project_launch_lifetime(window_lifetime *);
 
     window_lifetime_id const window_lifetime_id;
+
+    std::shared_ptr<project_launcher> const launcher;
 };
 }  // namespace yas::ae
