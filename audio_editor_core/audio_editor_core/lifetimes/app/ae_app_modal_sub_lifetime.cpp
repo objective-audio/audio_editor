b795136730a90775a4734251866e8b324b63bf2c
Yuki Yasoshima <yukiyasos@gmail.com>
2022-07-12 12:55:08 +0900

    app_modal_sub_lifetime_kind

--- audio_editor_core/audio_editor_core/lifetimes/app/ae_app_modal_sub_lifetime.cpp
+++ audio_editor_core/audio_editor_core/lifetimes/app/ae_app_modal_sub_lifetime.cpp
@@ -19,3 +19,15 @@ std::shared_ptr<T> const &yas::get(std::optional<ae::app_modal_sub_lifetime> con
 
 template std::shared_ptr<project_setup_dialog_lifetime> const &yas::get(
     std::optional<ae::app_modal_sub_lifetime> const &);
+
+app_modal_sub_lifetime_kind yas::to_kind(std::optional<ae::app_modal_sub_lifetime> const &sub_lifetime) {
+    using kind = app_modal_sub_lifetime_kind;
+
+    if (!sub_lifetime.has_value()) {
+        return kind::none;
+    } else if (std::holds_alternative<std::shared_ptr<project_setup_dialog_lifetime>>(sub_lifetime.value())) {
+        return kind::project_setup_dialog;
+    } else {
+        throw std::invalid_argument("invalid sub_lifetime");
+    }
+}
