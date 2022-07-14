a9bd0bcb1687db7ac6534ab8b51867fbcd0fd0b0
Yuki Yasoshima <yukiyasos@gmail.com>
2022-07-14 23:09:53 +0900

    project_url

--- audio_editor_core/audio_editor_core/lifetimes/project/ae_project_lifetime.h
+++ audio_editor_core/audio_editor_core/lifetimes/project/ae_project_lifetime.h
@@ -63,7 +63,6 @@ struct project_lifetime final {
     window_lifetime_id const window_lifetime_id;
     project_format const project_format;
 
-    std::shared_ptr<ae::project_url> const project_url;
     std::shared_ptr<module_location_pool> const module_location_pool;
     std::shared_ptr<marker_location_pool> const marker_location_pool;
     std::shared_ptr<waveform_mesh_importer> const waveforms_mesh_importer;
