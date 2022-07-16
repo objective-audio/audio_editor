c2e1e92014143ef4d2c4d9fd1b6829dd546447ff
Yuki Yasoshima <yukiyasos@gmail.com>
2022-07-16 11:26:41 +0900

    fix tests

--- audio_editor_tests/ae_pasting_file_module_tests.mm
+++ audio_editor_tests/ae_pasting_file_module_tests.mm
@@ -17,21 +17,22 @@ using namespace yas::ae;
 
 - (void)test_data {
     pasting_file_module const module{
-        .name = "test-name-1", .file_frame = -1, .length = 2, .file_name = "test-file-name-1"};
+        .name = "test-name-1", .file_frame = -1, .range = {2, 3}, .file_name = "test-file-name-1"};
 
-    XCTAssertEqual(module.data(), "{\"kind\":\"file_module\",\"length\":2,\"file_name\":\"test-file-name-1\","
-                                  "\"name\":\"test-name-1\",\"file_frame\":-1}");
+    XCTAssertEqual(module.data(),
+                   "{\"frame\":2,\"kind\":\"file_module\",\"length\":3,\"file_name\":\"test-file-name-1\","
+                   "\"name\":\"test-name-1\",\"file_frame\":-1}");
 }
 
 - (void)test_make {
     auto const module =
         pasting_file_module::make_value("{\"file_frame\":-10,\"file_name\":\"test-file-name-2\",\"kind\":\"file_"
-                                        "module\",\"length\":20,\"name\":\"test-name-2\"}");
+                                        "module\",\"frame\":20,\"length\":30,\"name\":\"test-name-2\"}");
 
     XCTAssertTrue(module.has_value());
     XCTAssertEqual(module.value().name, "test-name-2");
     XCTAssertEqual(module.value().file_frame, -10);
-    XCTAssertEqual(module.value().length, 20);
+    XCTAssertEqual(module.value().range, time::range(20, 30));
     XCTAssertEqual(module.value().file_name, "test-file-name-2");
 
     XCTAssertFalse(pasting_file_module::make_value("").has_value());
@@ -39,14 +40,15 @@ using namespace yas::ae;
 
 - (void)test_to_string {
     pasting_file_module const module{
-        .name = "test-name-3", .file_frame = 100, .length = 200, .file_name = "test-file-name-3"};
+        .name = "test-name-3", .file_frame = 100, .range = {200, 300}, .file_name = "test-file-name-3"};
 
-    XCTAssertEqual(to_string(module), "{name:test-name-3, file_frame:100, length:200, file_name:test-file-name-3}");
+    XCTAssertEqual(to_string(module),
+                   "{name:test-name-3, file_frame:100, range:{200, 300}, file_name:test-file-name-3}");
 }
 
 - (void)test_ostream {
     pasting_file_module const value{
-        .name = "test-name-4", .file_frame = 1000, .length = 2000, .file_name = "test-file-name-4"};
+        .name = "test-name-4", .file_frame = 1000, .range = {2000, 3000}, .file_name = "test-file-name-4"};
 
     std::ostringstream stream;
     stream << value;
