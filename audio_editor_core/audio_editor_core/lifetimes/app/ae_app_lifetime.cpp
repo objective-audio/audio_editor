88b63e34205dae773fb57259c12cefa24f51bdde
Yuki Yasoshima <yukiyasos@gmail.com>
2022-07-16 17:29:18 +0900

    app_lifetime generator

--- audio_editor_core/audio_editor_core/lifetimes/app/ae_app_lifetime.cpp
+++ audio_editor_core/audio_editor_core/lifetimes/app/ae_app_lifetime.cpp
@@ -12,8 +12,10 @@
 #include <audio_editor_core/ae_file_importer.h>
 #include <audio_editor_core/ae_file_info_loader.h>
 #include <audio_editor_core/ae_hierarchy.h>
+#include <audio_editor_core/ae_id_generator.h>
 #include <audio_editor_core/ae_system_url.h>
 #include <audio_editor_core/ae_ui_root_lifecycle.h>
+#include <audio_editor_core/ae_uuid_generator.h>
 #include <audio_editor_core/ae_window_lifecycle.h>
 #include <audio_editor_core/ae_window_opener.h>
 
@@ -30,15 +32,16 @@ std::shared_ptr<app_lifetime> app_lifetime::make_shared() {
     return std::make_shared<app_lifetime>(
         worker, system_url, app_launcher::make_shared(worker, system_url),
         file_importer::make_shared(worker, static_cast<uint32_t>(worker_priority::file_importing)), file_info_loader,
-        ae::color::make_shared(), window_lifecycle, app_modal_lifecycle::make_shared(),
-        ui_root_lifecycle::make_shared(), window_opener::make_shared(file_info_loader.get(), window_lifecycle.get()),
-        action_sender);
+        ae::color::make_shared(), uuid_generator::make_shared(), id_generator::make_shared(), window_lifecycle,
+        app_modal_lifecycle::make_shared(), ui_root_lifecycle::make_shared(),
+        window_opener::make_shared(file_info_loader.get(), window_lifecycle.get()), action_sender);
 }
 
 app_lifetime::app_lifetime(
     std::shared_ptr<yas::worker> const &worker, std::shared_ptr<ae::system_url> const &system_url,
     std::shared_ptr<ae::app_launcher> const &launcher, std::shared_ptr<ae::file_importer> const &file_importer,
     std::shared_ptr<ae::file_info_loader> const &file_info_loader, std::shared_ptr<ae::color> const &color,
+    std::shared_ptr<ae::uuid_generator> const &uuid_generator, std::shared_ptr<ae::id_generator> const &id_generator,
     std::shared_ptr<ae::window_lifecycle> const &window_lifecycle,
     std::shared_ptr<app_modal_lifecycle> const &dialog_lifecycle,
     std::shared_ptr<ae::ui_root_lifecycle> const &ui_root_lifecycle,
@@ -49,6 +52,8 @@ app_lifetime::app_lifetime(
       file_importer(file_importer),
       file_info_loader(file_info_loader),
       color(color),
+      uuid_generator(uuid_generator),
+      id_generator(id_generator),
       window_lifecycle(window_lifecycle),
       dialog_lifecycle(dialog_lifecycle),
       ui_root_lifecycle(ui_root_lifecycle),
