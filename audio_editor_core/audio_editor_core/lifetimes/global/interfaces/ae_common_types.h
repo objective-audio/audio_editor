45a096d815f31f30c3cd557ad9c1101c8cd25d05
Yuki Yasoshima <yukiyasos@gmail.com>
2022-09-10 11:41:14 +0900

    object_id

--- audio_editor_core/audio_editor_core/lifetimes/global/interfaces/ae_common_types.h
+++ audio_editor_core/audio_editor_core/lifetimes/global/interfaces/ae_common_types.h
@@ -4,6 +4,7 @@
 
 #pragma once
 
+#include <db/yas_db_object_id.h>
 #include <processing/yas_processing_common_types.h>
 #include <processing/yas_processing_time.h>
 
@@ -12,4 +13,6 @@ using frame_index_t = proc::frame_index_t;
 using length_t = proc::length_t;
 using sample_rate_t = proc::sample_rate_t;
 using time = proc::time;
+
+using object_id = db::object_id;
 }  // namespace yas::ae
