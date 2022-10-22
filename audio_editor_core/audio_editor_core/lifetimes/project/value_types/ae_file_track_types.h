5bc6d0a278bfa8dcd084aa8ddea4640dc6667107
Yuki Yasoshima <yukiyasos@gmail.com>
2022-10-22 11:37:52 +0900

    remove unused code

--- audio_editor_core/audio_editor_core/lifetimes/project/value_types/ae_file_track_types.h
+++ audio_editor_core/audio_editor_core/lifetimes/project/value_types/ae_file_track_types.h
@@ -11,8 +11,6 @@
 namespace yas::ae {
 using file_track_module_map_t = std::map<file_module_index, file_module_object>;
 
-static file_track_module_map_t const empty_file_track_modules;
-
 enum class file_track_event_type {
     any,
     reverted,
