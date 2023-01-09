48fe8acac8676509481cad41f6c2c7529f980967
Yuki Yasoshima <yukiyasos@gmail.com>
2023-01-09 10:17:32 +0900

    rename zooming

--- audio_editor_core/audio_editor_core/interface_adapters/zooming_presenter/ae_zooming_presenter.h
+++ audio_editor_core/audio_editor_core/interface_adapters/zooming_presenter/ae_zooming_presenter.h
@@ -1,5 +1,5 @@
 //
-//  ae_track_presenter.h
+//  ae_zooming_presenter.h
 //
 
 #pragma once
@@ -13,10 +13,10 @@ namespace yas::ae {
 class player;
 class zooming_pair;
 
-struct track_presenter final {
-    [[nodiscard]] static std::shared_ptr<track_presenter> make_shared(window_lifetime_id const &);
+struct zooming_presenter final {
+    [[nodiscard]] static std::shared_ptr<zooming_presenter> make_shared(window_lifetime_id const &);
 
-    track_presenter(std::shared_ptr<zooming_pair> const &);
+    zooming_presenter(std::shared_ptr<zooming_pair> const &);
 
     [[nodiscard]] ae::zooming_scale zooming_scale() const;
     [[nodiscard]] observing::syncable observe_zooming_scale(std::function<void(ae::zooming_scale const &)> &&);
@@ -24,9 +24,9 @@ struct track_presenter final {
    private:
     std::weak_ptr<zooming_pair> const _zooming_pair;
 
-    track_presenter(track_presenter const &) = delete;
-    track_presenter(track_presenter &&) = delete;
-    track_presenter &operator=(track_presenter const &) = delete;
-    track_presenter &operator=(track_presenter &&) = delete;
+    zooming_presenter(zooming_presenter const &) = delete;
+    zooming_presenter(zooming_presenter &&) = delete;
+    zooming_presenter &operator=(zooming_presenter const &) = delete;
+    zooming_presenter &operator=(zooming_presenter &&) = delete;
 };
 }  // namespace yas::ae
---
+++ audio_editor_core/audio_editor_core/lifetimes/ui_base/features/zooming/ae_ui_zooming_constants.h
@@ -0,0 +1,10 @@
+//
+//  ae_ui_zooming_constants.h
+//
+
+#pragma once
+
+namespace yas::ae::ui_zooming_constants {
+float constexpr standard_width_per_sec = 100.0f;
+float constexpr standard_height = 100.0f;
+}  // namespace yas::ae::ui_zooming_constants
