390c6a45a1c6b158ce928e8516bf7d28738f3585
Yuki Yasoshima <yukiyasos@gmail.com>
2023-02-27 19:40:29 +0900

    color tests

--- audio_editor_tests/ae_color_tests.mm
+++ audio_editor_tests/ae_color_tests.mm
@@ -25,10 +25,12 @@ using namespace yas::ae;
 
     XCTAssertNoThrow(color->module_frame());
     XCTAssertNoThrow(color->module_bg());
+    XCTAssertNoThrow(color->selected_module_bg());
     XCTAssertNoThrow(color->module_name());
     XCTAssertNoThrow(color->waveform());
 
-    XCTAssertNoThrow(color->selected_module_frame());
+    XCTAssertNoThrow(color->range_selection_fill());
+    XCTAssertNoThrow(color->range_selection_frame());
 
     XCTAssertNoThrow(color->pasting_module_frame());
 
@@ -36,6 +38,7 @@ using namespace yas::ae;
     XCTAssertNoThrow(color->edge_text());
 
     XCTAssertNoThrow(color->marker_square());
+    XCTAssertNoThrow(color->selected_marker_square());
     XCTAssertNoThrow(color->marker_line());
     XCTAssertNoThrow(color->marker_text());
 
