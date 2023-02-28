73cb0c39daf52775157fd41fd276b6e42ce958c1
Yuki Yasoshima <yukiyasos@gmail.com>
2023-02-28 22:31:18 +0900

    add markers_controller

---
+++ audio_editor_core/audio_editor_core/interface_adapters/markers_controller/ae_markers_controller.cpp
@@ -0,0 +1,31 @@
+//
+//  ae_markers_controller.cpp
+//
+
+#include "ae_markers_controller.hpp"
+
+#include <audio_editor_core/ae_hierarchy.h>
+#include <audio_editor_core/ae_project_action_sender.h>
+#include <cpp_utils/yas_assertion.h>
+
+using namespace yas;
+using namespace yas::ae;
+
+std::shared_ptr<markers_controller> markers_controller::make_shared(window_lifetime_id const &lifetime_id) {
+    auto const &project_lifetime = hierarchy::project_lifetime_for_id(lifetime_id);
+    return std::make_shared<markers_controller>(project_lifetime->action_sender);
+}
+
+markers_controller::markers_controller(std::shared_ptr<project_action_sender> const &action_sender)
+    : _action_sender(action_sender) {
+}
+
+void markers_controller::select(std::vector<marker_index> const &marker_indices) {
+    auto const action_sender = this->_action_sender.lock();
+    if (!action_sender) {
+        assertion_failure_if_not_test();
+        return;
+    }
+
+    action_sender->send(editing_action_name::select_markers, marker_indices);
+}
---
+++ audio_editor_core/audio_editor_core/interface_adapters/markers_controller/ae_markers_controller.hpp
@@ -0,0 +1,29 @@
+//
+//  ae_markers_controller.hpp
+//
+
+#pragma once
+
+#include <audio_editor_core/ae_window_lifetime_id.h>
+
+#include <audio_editor_core/ae_marker_index.hpp>
+
+namespace yas::ae {
+class project_action_sender;
+
+struct markers_controller final {
+    [[nodiscard]] static std::shared_ptr<markers_controller> make_shared(window_lifetime_id const &);
+
+    markers_controller(std::shared_ptr<project_action_sender> const &);
+
+    void select(std::vector<marker_index> const &);
+
+   private:
+    std::weak_ptr<project_action_sender> const _action_sender;
+
+    markers_controller(markers_controller const &) = delete;
+    markers_controller(markers_controller &&) = delete;
+    markers_controller &operator=(markers_controller const &) = delete;
+    markers_controller &operator=(markers_controller &&) = delete;
+};
+}  // namespace yas::ae
