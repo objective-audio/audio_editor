135d05629a47840a96047413d19edf68b804285b
Yuki Yasoshima <yukiyasos@gmail.com>
2023-02-28 15:01:46 +0900

    deselector

--- audio_editor_core/audio_editor_core/lifetimes/project/features/ae_deselector.hpp
+++ audio_editor_core/audio_editor_core/lifetimes/project/features/ae_deselector.hpp
@@ -4,11 +4,13 @@
 
 #pragma once
 
+#include <audio_editor_core/ae_range_selector_dependency.h>
+
 namespace yas::ae {
 class selected_module_pool;
 class selected_marker_pool;
 
-struct deselector final {
+struct deselector final : deselector_for_range_selector {
     deselector(selected_module_pool *, selected_marker_pool *);
 
     void deselect_all();
