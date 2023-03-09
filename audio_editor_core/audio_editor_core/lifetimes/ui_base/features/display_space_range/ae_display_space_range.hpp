075b7b5ece3a1c159fcc59c042b071b46572a226
Yuki Yasoshima <yukiyasos@gmail.com>
2023-03-09 14:35:21 +0900

    add display_space_range

---
+++ audio_editor_core/audio_editor_core/lifetimes/ui_base/features/display_space_range/ae_display_space_range.hpp
@@ -0,0 +1,33 @@
+//
+//  ae_display_space_range.hpp
+//
+
+#pragma once
+
+#include <audio_editor_core/ae_common_types.h>
+#include <audio_editor_core/ae_project_format.h>
+#include <audio_editor_core/ae_window_lifetime_id.h>
+
+namespace yas::ae {
+class display_space;
+class player;
+
+struct display_space_range final {
+    display_space_range(project_format const, display_space const *, player const *);
+
+    [[nodiscard]] std::optional<time::range> current() const;
+    [[nodiscard]] std::optional<time::range> zero() const;
+
+   private:
+    project_format const _project_format;
+    display_space const *const _display_space;
+    player const *const _player;
+
+    display_space_range(display_space_range const &) = delete;
+    display_space_range(display_space_range &&) = delete;
+    display_space_range &operator=(display_space_range const &) = delete;
+    display_space_range &operator=(display_space_range &&) = delete;
+
+    std::optional<time::range> _range(frame_index_t const frame) const;
+};
+}  // namespace yas::ae
