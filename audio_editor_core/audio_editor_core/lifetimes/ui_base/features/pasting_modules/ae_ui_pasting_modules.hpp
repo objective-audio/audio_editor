611da8d69c1d911fd4e45b9d51a14db609f54694
Yuki Yasoshima <yukiyasos@gmail.com>
2023-04-17 22:20:33 +0900

    ui_pasting_modules

--- audio_editor_core/audio_editor_core/lifetimes/ui_base/features/pasting_modules/ae_ui_pasting_modules.hpp
+++ audio_editor_core/audio_editor_core/lifetimes/ui_base/features/pasting_modules/ae_ui_pasting_modules.hpp
@@ -4,34 +4,32 @@
 
 #pragma once
 
+#include <audio_editor_core/ae_ui_types.h>
 #include <audio_editor_core/ae_window_lifetime_id.h>
+#include <cpp_utils/yas_index_range.h>
 #include <ui/yas_ui_umbrella.h>
 
 namespace yas::ae {
 class pasting_module_content;
 class pasting_modules_presenter;
-class display_space;
 class color;
+template <typename V, typename I>
+class dynamic_mesh_container;
 
 struct ui_pasting_modules final {
-    [[nodiscard]] static std::shared_ptr<ui_pasting_modules> make_shared(window_lifetime_id const &,
-                                                                         std::shared_ptr<ui::node> const &);
+    [[nodiscard]] static std::shared_ptr<ui_pasting_modules> make_shared(window_lifetime_id const &, ui::node *);
 
     ui_pasting_modules(std::shared_ptr<pasting_modules_presenter> const &, std::shared_ptr<ui::standard> const &,
-                       std::shared_ptr<ui::node> const &, ae::color *);
+                       ui::node *, ae::color *);
 
     void set_scale(ui::size const &);
 
    private:
     std::shared_ptr<pasting_modules_presenter> const _presenter;
+    ui::node *const _node;
     ae::color *const _color;
-    std::shared_ptr<ui::node> const _frame_node;
 
-    ui::size _scale = ui::size::one();
-    std::size_t _remaked_count = 0;
-    std::shared_ptr<ui::dynamic_mesh_vertex_data> _vertex_data;
-    std::shared_ptr<ui::dynamic_mesh_index_data> _frame_index_data;
-    std::shared_ptr<ui::mesh> const _frame_mesh;
+    std::unique_ptr<dynamic_mesh_container<vertex2d_rect, frame_index2d_rect>> const _mesh_container;
 
     observing::canceller_pool _pool;
 
@@ -44,8 +42,5 @@ struct ui_pasting_modules final {
     void _update(std::size_t const count, std::vector<std::pair<std::size_t, pasting_module_content>> const &inserted,
                  std::vector<std::pair<std::size_t, pasting_module_content>> const &replaced,
                  std::vector<std::pair<std::size_t, pasting_module_content>> const &erased);
-
-    void _remake_data_if_needed(std::size_t const max_count);
-    void _set_rect_count(std::size_t const rect_count);
 };
 }  // namespace yas::ae
