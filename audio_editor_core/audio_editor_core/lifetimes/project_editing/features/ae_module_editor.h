29f743a89df9535213694cc6b8e169f13799b80d
Yuki Yasoshima <yukiyasos@gmail.com>
2023-09-23 12:30:37 +0900

    nodiscard

--- audio_editor_core/audio_editor_core/lifetimes/project_editing/features/ae_module_editor.h
+++ audio_editor_core/audio_editor_core/lifetimes/project_editing/features/ae_module_editor.h
@@ -59,7 +59,7 @@ struct module_editor final {
     module_editor &operator=(module_editor const &) = delete;
     module_editor &operator=(module_editor &&) = delete;
 
-    bool _has_target_modules() const;
+    [[nodiscard]] bool _has_target_modules() const;
     void _erase_modules(selected_module_set &&);
 };
 }  // namespace yas::ae
