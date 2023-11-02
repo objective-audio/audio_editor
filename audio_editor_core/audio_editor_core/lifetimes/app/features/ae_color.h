eec2f7e5551fc267b83d0dcfdf4c7190b1be4937
Yuki Yasoshima <yukiyasos@gmail.com>
2023-11-02 20:51:53 +0900

    track text color

--- audio_editor_core/audio_editor_core/lifetimes/app/features/ae_color.h
+++ audio_editor_core/audio_editor_core/lifetimes/app/features/ae_color.h
@@ -46,6 +46,8 @@ struct color final {
     [[nodiscard]] ui::color selected_track_square() const;
     [[nodiscard]] ui::color track_bg() const;
     [[nodiscard]] ui::color selected_track_bg() const;
+    [[nodiscard]] ui::color track_text() const;
+    [[nodiscard]] ui::color selected_track_text() const;
 
     [[nodiscard]] ui::color modal_bg() const;
 
