16ff3a3c5c17ade86e67a30cd427754f19bd1b8f
Yuki Yasoshima <yukiyasos@gmail.com>
2023-11-02 16:27:09 +0900

    fix ui_markers

--- audio_editor_core/audio_editor_core/lifetimes/ui_base/features/markers/ae_ui_markers.mm
+++ audio_editor_core/audio_editor_core/lifetimes/ui_base/features/markers/ae_ui_markers.mm
@@ -211,14 +211,11 @@ ui_markers::ui_markers(project_lifetime_id const &project_lifetime_id,
 void ui_markers::_replace_data() {
     auto const &contents = this->_presenter->contents();
     auto const &names = this->_names;
-    auto const normal_name_color = this->_color->marker_text();
-    auto const selected_name_color = this->_color->selected_marker_text();
 
     this->_set_rect_count(contents.size());
 
     this->_square_fill_mesh_container->write_vertex_elements(
-        [this, &contents, &names, &normal_name_color, &selected_name_color](index_range const range,
-                                                                            vertex2d_rect *vertex_rects) {
+        [this, &contents, &names](index_range const range, vertex2d_rect *vertex_rects) {
             if (contents.size() <= range.index) {
                 return;
             }
@@ -239,7 +236,6 @@ void ui_markers::_replace_data() {
 
                 if (content.has_value()) {
                     name->set_content(content.value(), this->_square_region_updating(content_idx));
-                    name->update_color(selected_name_color, normal_name_color);
 
                     rect.set_tex_coord(filled_tex_coords);
 
@@ -418,24 +414,24 @@ void ui_markers::_update_data(std::set<std::size_t> const &inserted, std::set<st
 }
 
 void ui_markers::_set_rect_count(std::size_t const rect_count) {
-    auto const prev_rect_count = this->_names.size();
+    auto const prev_names_count = this->_names.size();
     auto const reserving_count = common_utils::reserving_count(rect_count, ui_markers_constants::reserving_interval);
 
     this->_square_fill_mesh_container->set_element_count(rect_count);
     this->_line_mesh_container->set_element_count(rect_count);
 
-    if (prev_rect_count < rect_count) {
+    if (prev_names_count < rect_count) {
         this->_names.reserve(reserving_count);
 
-        auto each = make_fast_each(rect_count - prev_rect_count);
+        auto each = make_fast_each(rect_count - prev_names_count);
         while (yas_each_next(each)) {
             this->_names.emplace_back(
                 ui_marker_name::make_unique(this->_project_lifetime_id, this->_names_root_node.get()));
         }
-    } else if (rect_count < prev_rect_count) {
-        auto each = make_fast_each(prev_rect_count - rect_count);
+    } else if (rect_count < prev_names_count) {
+        auto each = make_fast_each(prev_names_count - rect_count);
         while (yas_each_next(each)) {
-            auto const idx = prev_rect_count - 1 - yas_each_index(each);
+            auto const idx = prev_names_count - 1 - yas_each_index(each);
             this->_names.at(idx)->finalize();
         }
         this->_names.resize(rect_count);
