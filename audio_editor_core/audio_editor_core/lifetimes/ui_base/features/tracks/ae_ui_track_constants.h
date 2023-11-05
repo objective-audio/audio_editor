7805fe9d51191a050ccba0c1efd4bcc102c089f5
Yuki Yasoshima <yukiyasos@gmail.com>
2023-11-05 11:19:20 +0900

    min square width

--- audio_editor_core/audio_editor_core/lifetimes/ui_base/features/tracks/ae_ui_track_constants.h
+++ audio_editor_core/audio_editor_core/lifetimes/ui_base/features/tracks/ae_ui_track_constants.h
@@ -2,6 +2,6 @@
 
 namespace yas::ae::ui_track_constants {
 static std::size_t constexpr reserving_interval = 64;
-static float constexpr min_square_width = 100.0f;
+static float constexpr min_square_width = 40.0f;
 static float constexpr square_height_rate = 0.95f;
 }  // namespace yas::ae::ui_track_constants
