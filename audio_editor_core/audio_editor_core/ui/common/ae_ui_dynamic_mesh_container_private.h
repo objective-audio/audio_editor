ba1fb3c4477cc16b0807ed528a25759566dd45a1
Yuki Yasoshima <yukiyasos@gmail.com>
2023-11-18 20:57:21 +0900

    dynamic_mesh_container reducing

--- audio_editor_core/audio_editor_core/ui/common/ae_ui_dynamic_mesh_container_private.h
+++ audio_editor_core/audio_editor_core/ui/common/ae_ui_dynamic_mesh_container_private.h
@@ -5,12 +5,18 @@
 #pragma once
 
 #include <audio_editor_core/ae_common_utils.h>
+#include <cpp_utils/yas_stl_utils.h>
 
 namespace yas::ae {
 template <typename VertexElement, typename IndexElement>
-dynamic_mesh_container<VertexElement, IndexElement>::dynamic_mesh_container(std::size_t const interval,
-                                                                            make_content_f &&make_content)
-    : _element_count(0), _interval(interval), _make_content(std::move(make_content)) {
+dynamic_mesh_container<VertexElement, IndexElement>::dynamic_mesh_container(
+    std::size_t const interval, make_content_f &&make_content, std::shared_ptr<system_time_providable> const &provider,
+    duration_t const reducing_duration)
+    : _element_count(0),
+      _interval(interval),
+      _make_content(std::move(make_content)),
+      _system_time_provider(provider),
+      _reducing_duration(reducing_duration) {
 }
 
 template <typename VertexElement, typename IndexElement>
@@ -21,8 +27,20 @@ void dynamic_mesh_container<VertexElement, IndexElement>::set_element_count(std:
 
     this->_element_count = element_count;
 
-    auto const reserving_count = common_utils::reserving_count(element_count, this->_interval);
-    this->_remake_contents_if_needed(reserving_count / this->_interval);
+    auto const reserving_element_count = common_utils::reserving_count(element_count, this->_interval);
+    std::size_t const contents_count = reserving_element_count / this->_interval;
+    std::size_t const prev_contents_count = this->_contents.size();
+
+    if (prev_contents_count < contents_count) {
+        this->_remake_contents_if_needed(contents_count);
+
+        this->_reducings = filter(this->_reducings, [&contents_count](dynamic_mesh_reducing const &reducing) {
+            return contents_count < reducing.contents_count;
+        });
+    } else if (contents_count < prev_contents_count) {
+        this->_reducings.emplace_back(
+            dynamic_mesh_reducing{.contents_count = contents_count, .time_point = this->_system_time_provider->now()});
+    }
 
     auto const calc_length = [](std::size_t const idx, std::size_t const interval, std::size_t const element_count) {
         auto const top_element_idx = interval * idx;
@@ -56,22 +74,21 @@ std::size_t dynamic_mesh_container<VertexElement, IndexElement>::element_count()
 template <typename VertexElement, typename IndexElement>
 void dynamic_mesh_container<VertexElement, IndexElement>::write_vertex_elements(
     std::function<void(index_range const range, VertexElement *)> const &handler) {
-    if (this->_element_count == 0) {
+    auto const reserved_contents_count = this->_contents.size();
+    auto const available_contents_count = this->_available_contents_count();
+    if (available_contents_count == 0 || reserved_contents_count == 0) {
         return;
     }
 
-    std::size_t const element_mod_count = this->_element_count % this->_interval;
-    std::size_t const floored_contents_count = this->_element_count / this->_interval;
-    std::size_t const contents_count = floored_contents_count + (element_mod_count != 0 ? 1 : 0);
+    auto const contents_count = std::min(reserved_contents_count, available_contents_count);
 
     auto each = make_fast_each(contents_count);
     while (yas_each_next(each)) {
         std::size_t const &content_idx = yas_each_index(each);
         std::size_t const top_element_idx = this->_interval * content_idx;
-        std::size_t const length = (content_idx == floored_contents_count) ? element_mod_count : this->_interval;
 
         this->_contents.at(content_idx)
-            ->vertex_data->write([range = index_range{.index = top_element_idx, .length = length},
+            ->vertex_data->write([range = index_range{.index = top_element_idx, .length = this->_interval},
                                   &handler](std::vector<ui::vertex2d_t> &vertices) {
                 handler(range, (VertexElement *)vertices.data());
             });
@@ -93,6 +110,42 @@ void dynamic_mesh_container<VertexElement, IndexElement>::write_vertex_element(
             auto *elements = (VertexElement *)vertices.data();
             handler(&elements[idx_in_content]);
         });
+}
+
+template <typename VertexElement, typename IndexElement>
+void dynamic_mesh_container<VertexElement, IndexElement>::reduce_if_needed() {
+    auto const available_contents_count = this->_available_contents_count();
+    auto const reserved_contents_count = this->_contents.size();
+    auto const now = this->_system_time_provider->now();
+    auto threshold_time = now - this->_reducing_duration;
+
+    std::optional<dynamic_mesh_reducing> reducing = std::nullopt;
+
+    this->_reducings = filter(this->_reducings, [&threshold_time, &reducing, &available_contents_count,
+                                                 &reserved_contents_count](dynamic_mesh_reducing const &value) {
+        if (threshold_time < value.time_point) {
+            return true;
+        } else {
+            if (available_contents_count <= value.contents_count && value.contents_count < reserved_contents_count) {
+                if (reducing.has_value()) {
+                    if (value.contents_count < reducing.value().contents_count) {
+                        reducing = value;
+                    }
+                } else {
+                    reducing = value;
+                }
+            }
+            return false;
+        }
+    });
+
+    if (reducing.has_value()) {
+        std::size_t const contents_count = reducing.value().contents_count;
+
+        if (contents_count < this->_contents.size()) {
+            this->_contents.resize(contents_count);
+        }
+    }
 }
 
 template <typename VertexElement, typename IndexElement>
@@ -121,5 +174,10 @@ void dynamic_mesh_container<VertexElement, IndexElement>::_remake_contents_if_ne
             }
         });
     }
+}
+
+template <typename VertexElement, typename IndexElement>
+std::size_t dynamic_mesh_container<VertexElement, IndexElement>::_available_contents_count() const {
+    return common_utils::reserving_count(this->_element_count, this->_interval) / this->_interval;
 }
 }  // namespace yas::ae
